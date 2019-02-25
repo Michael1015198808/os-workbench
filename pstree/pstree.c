5733018eadd7eff34457a3f938d036c10525b843
tracer-nju <tracer@nju.edu.cn>
2019-02-25 21:41:25 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: e7a87cdac9f74491aaaac3245c3569b6   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  21:41:25 up  1:56,  4 users,  load average: 0.26, 0.25, 0.19

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -4,7 +4,45 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+void printdir(char *dir, int depth)
+{
+    DIR *dp;
+    struct dirent *entry;
+    struct stat statbuf;
+    if((dp = opendir(dir)) == NULL) {
+        fprintf(stderr,"cannot open directory: %s\n", dir);
+        return;
+    }
+    chdir(dir);
+    while((entry = readdir(dp)) != NULL) {
+        lstat(entry->d_name,&statbuf);
+        if(S_ISDIR(statbuf.st_mode)) {
+            /* Found a directory, but ignore . and .. */
+            if(strcmp(".",entry->d_name) == 0 ||
+                strcmp("..",entry->d_name) == 0)
+                continue;
+            printf("%*s%s/\n",depth,"",entry->d_name);
+            /* Recurse at a new indent level */
+            printdir(entry->d_name,depth+4);
+        }
+        else printf("%*s%s\n",depth,"",entry->d_name);
+    }
+    chdir("..");
+    closedir(dp);
+}
+
+int main()
+{
+    printf("Directory scan of /proc:\n");
+    printdir("/proc",0);
+    printf("done.\n");
+    exit(0);
+}
+//Copy from https://stackoverflow.com/questions/8149569/scan-a-directory-to-find-files-in-c
+/*int main(int argc, char *argv[]) {
   printf("Hello, World!\n");
   int i;
   for (i = 0; i < argc; i++) {
@@ -16,4 +54,4 @@ int main(int argc, char *argv[]) {
   getchar();//Give me time to find it in /proc
   assert(!argv[argc]); // always true
   return 0;
-}
+}*/
