4992c61fc7758672523725d6626aacfba9246a81
tracer-nju <tracer@nju.edu.cn>
2019-02-25 20:07:45 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: e7a87cdac9f74491aaaac3245c3569b6   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  20:07:45 up 22 min,  4 users,  load average: 0.31, 0.46, 0.46

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[]) {
   printf("Hello, World!\n");
@@ -8,6 +10,8 @@ int main(int argc, char *argv[]) {
     assert(argv[i]); // always true
     printf("argv[%d] = %s\n", i, argv[i]);
   }
+  printf("%d\n",getpid());
+  getchar();//Give me time to find it in /proc
   assert(!argv[argc]); // always true
   return 0;
 }
