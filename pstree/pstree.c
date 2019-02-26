ed6c018862022c2d3a924cbcca257201bd09b5e6
tracer-nju <tracer@nju.edu.cn>
2019-02-26 15:17:20 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: e7a87cdac9f74491aaaac3245c3569b6   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  15:17:20 up 19:32,  7 users,  load average: 0.25, 0.21, 0.24

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -49,13 +49,13 @@ void add_sonpro(Proc* pp,pid_t pid){
     if(pp->son==NULL){
         pp->son=info[pid];
     }else{
-        if(cmp(pp->son,info[pid])<=0){
+        if(cmp(pp->son,info[pid])>=0){
             info[pid]->bro=pp->son;
             pp->son=info[pid];
             return;
         }
         Proc *l=pp->son,*r=l->bro;
-        while(r!=NULL&&cmp(r,info[pid])>0){
+        while(r!=NULL&&cmp(r,info[pid])<0){
             l=r;
             r=l->bro;
         };
