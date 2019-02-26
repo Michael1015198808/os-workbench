e5419adc2c4c2d7f3df0cbf647b4b9395f1c66ed
tracer-nju <tracer@nju.edu.cn>
2019-02-27 00:42:29 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: a945f7ed4c744a43995c4a5e82489c85   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  00:42:29 up 11 min,  1 user,  load average: 0.19, 0.22, 0.13

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -7,13 +7,13 @@
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
-//#define IGNORE_PRO_EXIT
+#define IGNORE_PRO_EXIT
 //Ignore the processes that are not existing when open
 //comment it to allow error report
-#define test(_con,_fmt,...) \
+#define test(_con,...) \
 do{\
     if(!(_con)){\
-        fprintf(stderr,"\33[1;31mfixed by " _fmt "\33[0m",__VA_ARGS__);\
+        fprintf(stderr,__VA_ARGS__);\
         assert(0);\
     }\
 }while(0)
