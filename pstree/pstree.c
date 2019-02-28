11a0b2d07a3575e49ab0aa1995bd81b744205a94
tracer-nju <tracer@nju.edu.cn>
2019-02-28 08:05:42 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 1c910147067b446b892719b050b27c2a   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  08:05:42 up  9:48,  1 user,  load average: 0.43, 0.32, 0.22

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -177,7 +177,7 @@ void print_tree(const Proc const *p,int is_first){
             }
             printf("%s",bar_exist[i]!=0?" │":"  ");
         }
-        for(j=1;j<blank_len[depth];++j){
+        for(j=0;j<blank_len[depth];++j){
             putchar(' ');
         }
         if(p->bro!=NULL){printf(" ├─");}
@@ -201,7 +201,7 @@ void print_tree(const Proc const *p,int is_first){
     if(p->son==NULL){putchar('\n');return;}
 
 
-    blank_len[++depth]=len;
+    blank_len[++depth]=len+1;
     bar_exist[depth]=p->son->bro!=NULL;
     //print its sons
     print_tree(p->son,1);
