08ea5c6a503c3038ba3d822565229f3c2d51e95d
tracer-nju <tracer@nju.edu.cn>
2019-03-05 14:33:29 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 59e9980fb76d44eb8e5ec8100e67286f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  14:33:30 up 19:21,  6 users,  load average: 0.28, 0.24, 0.35

--- amgame/include/game.h
+++ amgame/include/game.h
@@ -1,8 +1,9 @@
 #include <am.h>
 #include <amdev.h>
+#include <klib.h>
 
 #define SIDE 16
 
-static inline void puts(const char *s) {
+/*static inline void puts(const char *s) {
   for (; *s; s++) _putc(*s);
-}
+}*/
--- amgame/src/game.c
+++ amgame/src/game.c
@@ -2,11 +2,13 @@
 
 void init_screen();
 void splash();
-void read_key();
-typedef uint32_t pixel[1][1];
-
+//void read_key();
+typedef uint32_t pixel_t;
+pixel_t color[6][6];
 int main() {
   // Operating system is a C program
+  srand(uptime(NULL));
+  printf("Hello World from " __ISA__ " program!\n");
   _ioe_init();
   init_screen();
   splash();
@@ -16,7 +18,7 @@ int main() {
   return 0;
 }
 
-void read_key() {
+/*void read_key() {
   _DEV_INPUT_KBD_t event = { .keycode = _KEY_NONE };
   #define KEYNAME(key) \
     [_KEY_##key] = #key,
@@ -29,7 +31,7 @@ void read_key() {
     puts(key_names[event.keycode]);
     puts("\n");
   }
-}
+}*/
 
 int w, h;
 
@@ -40,7 +42,7 @@ void init_screen() {
   h = info.height;
 }
 
-void draw_rect(int x, int y, int w, int h, uint32_t color) {
+void mono_rect(int x, int y, int w, int h, uint32_t color) {
   uint32_t pixels[w * h]; // WARNING: allocated on stack
   _DEV_VIDEO_FBCTL_t event = {
     .x = x, .y = y, .w = w, .h = h, .sync = 1,
@@ -56,7 +58,7 @@ void splash() {
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
       if ((x & 1) ^ (y & 1)) {
-        draw_rect(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
+        mono_rect(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
       }
     }
   }
