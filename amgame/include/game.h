42aee812356c6110d54a864708c36d78d39e0087
tracer-nju <tracer@nju.edu.cn>
2019-03-05 15:35:31 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 59e9980fb76d44eb8e5ec8100e67286f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  15:35:31 up 20:23,  7 users,  load average: 1.38, 0.95, 0.64

--- amgame/include/game.h
+++ amgame/include/game.h
@@ -4,6 +4,8 @@
 
 #define SIDE 16
 
+void mono_rect(int x, int y, int w, int h, uint32_t color);
+void draw_str(char* s,int x,int y,size_t size,int color);
 /*static inline void puts(const char *s) {
   for (; *s; s++) _putc(*s);
 }*/
