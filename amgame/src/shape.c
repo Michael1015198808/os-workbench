0296907f36f593e31d020726a5b3a47b84015d83
tracer-nju <tracer@nju.edu.cn>
2019-03-05 18:01:29 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 59e9980fb76d44eb8e5ec8100e67286f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  18:01:29 up 22:49,  7 users,  load average: 1.13, 0.82, 0.73

--- amgame/src/shape.c
+++ amgame/src/shape.c
@@ -38,4 +38,7 @@ void mono_rect(int x, int y, int w, int h, uint32_t color) {
 void draw_grid(int x,int y){
   extern pixel color[GRID_NUM][GRID_NUM];
   mono_rect((x+MARGIN) * SIDE*3, (y+MARGIN) * SIDE*3, SIDE*3, SIDE*3, color[x][y].val);
+  if(color[x][y].alpha==1){
+      draw_cross(SIDE/2+coor_to_pix(x,y)+SIDE/2,SIDE,0xffffff,0x00000000);
+  }
 }
