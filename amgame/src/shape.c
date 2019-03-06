017441ac3b4d97f635c82595c323114e6934bd96
tracer-nju <tracer@nju.edu.cn>
2019-03-06 11:31:22 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 59e9980fb76d44eb8e5ec8100e67286f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:31:22 up 1 day, 16:19,  7 users,  load average: 0.72, 0.73, 0.93

--- amgame/src/shape.c
+++ amgame/src/shape.c
@@ -51,7 +51,7 @@ void draw_arrow(int x,int y,int color,Direc direc){
   printf("%d\n",direc);
   if(direc&2){
     for(i=0;i<3;++i){
-      mono_rect((x+MARGIN) * SIDE*3+SIDE+i*SIDE/2, (y+MARGIN) * SIDE*3, SIDE/2, SIDE/2, color);
+      mono_rect((x+MARGIN) * SIDE*3+(i+1)*SIDE/2, (y+MARGIN) * SIDE*3+SIDE/2, SIDE/2, SIDE/2, color);
     }
   }else{
     *(int*)0=0;
