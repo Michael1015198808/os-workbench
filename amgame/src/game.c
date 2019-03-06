197d737957e7cdac00cee8377f06663ca3a6cb5e
tracer-nju <tracer@nju.edu.cn>
2019-03-06 17:08:25 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 59e9980fb76d44eb8e5ec8100e67286f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  17:08:25 up 1 day, 21:56,  4 users,  load average: 0.52, 0.59, 0.61

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -24,8 +24,10 @@ int main() {
   draw_str("Swap the tiles to put the colors in order!",0,32,2,0x3fff00);
   draw_str("Press h for hint",0,h-2*8,2,0x3fff00);
   while (1) {
+      printf("%d\n",old_time);
     do{
      new_time=uptime();
+     printf("%d\n",new_time);
     }while(new_time<(old_time+10));
     old_time=new_time;
     if(print_flag==1){
