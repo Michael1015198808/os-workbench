ab6587e522720ba04d6165c4a9b5920aab6dbb4c
tracer-nju <tracer@nju.edu.cn>
2019-03-06 17:18:44 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 59e9980fb76d44eb8e5ec8100e67286f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  17:18:44 up 1 day, 22:06,  4 users,  load average: 0.62, 0.86, 0.76

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -19,9 +19,10 @@ int main() {
   _ioe_init();
   init_screen();
   init();
-  draw_str("Move with arrow keys\nSelect grid with space key\n"
-            "Swap the tiles to put the colors in order!\n"
-            "Press h for hint",0,0,2,0x3fff00);
+  draw_str("Move with arrow keys\n"
+           "Select grid with space key\n"
+           "Swap the tiles to put the colors in order!\n",0,0,2,0x3fff00);
+  draw_str("Press h for hint",0,h-2*8,2,0x3fff00);
   while (1) {
     do{
      new_time=uptime();
