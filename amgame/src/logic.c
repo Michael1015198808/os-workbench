9a672d5570a95716395e14427e1137749beaaa11
tracer-nju <tracer@nju.edu.cn>
2019-03-07 00:10:21 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 5ab2163fdb8c484087e64c1a117acb6d   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  00:10:21 up  1:53,  3 users,  load average: 0.41, 0.46, 0.45

--- amgame/src/logic.c
+++ amgame/src/logic.c
@@ -39,7 +39,7 @@ void init(void){
   //draw_str("Move with arrow keys\n"
            //"Select with space key\n",0,0,2,0x3fff00);
   draw_str("Swap to put the colors in order!",0,h-8*8,2,0x3fff00);
-  draw_str("Press h for hints",w-5*8,0,2,0x3fff00);
+  draw_str("Press h for hints",w-20*8,0,2,0x3fff00);
 //random shuffle
   for(int i=0;i<50;++i){
       int j,k;
