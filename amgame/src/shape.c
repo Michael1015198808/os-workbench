ed32ac1e7bacc443f86383dfe1ca9f6c298ba456
tracer-nju <tracer@nju.edu.cn>
2019-03-06 11:52:51 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 59e9980fb76d44eb8e5ec8100e67286f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:52:51 up 1 day, 16:40,  3 users,  load average: 0.65, 0.88, 0.92

--- amgame/src/shape.c
+++ amgame/src/shape.c
@@ -48,11 +48,13 @@ extern int choosen_idx,choosen[2];
 }
 void draw_arrow(int x,int y,int color,Direc direc){
   int i;
+  char* s[]={"left","right","up","down"};
+  printf("%s\n",s[direc]);
   if(direc&2){
       mono_rect((x+MARGIN) * SIDE*3+(SIDE*5)/4, (y+MARGIN) * SIDE*3+(SIDE*3)/4, SIDE/2, (SIDE*3)/2, color);
     for(i=1;i<4;++i){
-      mono_rect((x+MARGIN) * SIDE*3+(6-i)*SIDE/4, (y+MARGIN) * SIDE*3+((10-(direc&1?i:-i))*SIDE)/4, SIDE/4, SIDE/4, color);
-      mono_rect((x+MARGIN) * SIDE*3+(5+i)*SIDE/4, (y+MARGIN) * SIDE*3+((10-(direc&1?i:-i))*SIDE)/4, SIDE/4, SIDE/4, color);
+      mono_rect((x+MARGIN) * SIDE*3+(6-i)*SIDE/4, (y+MARGIN) * SIDE*3+((10-i)*SIDE)/4, SIDE/4, SIDE/4, color);
+      mono_rect((x+MARGIN) * SIDE*3+(5+i)*SIDE/4, (y+MARGIN) * SIDE*3+((10-i)*SIDE)/4, SIDE/4, SIDE/4, color);
     }
   }else{
       mono_rect((x+MARGIN) * SIDE*3+(SIDE*3)/4, (y+MARGIN) * SIDE*3+(SIDE*5)/4, (SIDE*3)/2, SIDE/2, color);
