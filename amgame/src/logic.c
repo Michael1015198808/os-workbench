978b88619357496977b8cfc36c8509915164dbcb
tracer-nju <tracer@nju.edu.cn>
2019-03-07 09:03:41 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 5d13c9f1f9c34b4a9512cd8204b54ff4   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  09:03:41 up  1:06,  1 user,  load average: 0.45, 0.47, 0.68

--- amgame/src/logic.c
+++ amgame/src/logic.c
@@ -81,7 +81,7 @@ void swap_pixel(void){
   //printf("%d,%d\n",i,j);
   if((i==GRID_NUM)&&(j==GRID_NUM)){
     stat=GAME_WIN;
-    draw_str("You Win!\npress r to restart",h/2,w/2,SIDE/8,0x3fff00);
+    draw_str("You Win!\npress r to restart",h/2-SIDE/4,w/2-SIDE*5,SIDE/8,0x3fff00);
   }
 }
 
