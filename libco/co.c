918a4a243817b024aa9a584036f3c63cfbb98874
tracer-nju <tracer@nju.edu.cn>
2019-03-16 01:25:50 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: d67bbcf1de0545d2952e2bf0a075cd59   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  01:25:50 up 2 days, 16:08,  7 users,  load average: 0.13, 0.15, 0.18

--- libco/co.c
+++ libco/co.c
@@ -49,7 +49,7 @@ static jmp_buf ret_buf;
 struct co* co_start(const char *name, func_t func, void *arg) {
   get_sp(__stack_backup);
   current=new_co();
-  set_sp(current->stack+STACK_SIZE);
+  //set_sp(current->stack+STACK_SIZE);
   if(!setjmp(ret_buf)){
       func(arg);
   }
