7aa19a3e2531ef61e9808a6cb1d1018920ef1af2
tracer-nju <tracer@nju.edu.cn>
2019-03-16 20:08:03 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: d67bbcf1de0545d2952e2bf0a075cd59   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  20:08:03 up 3 days, 10:50,  8 users,  load average: 0.38, 0.47, 0.44

--- libco/co.c
+++ libco/co.c
@@ -22,10 +22,15 @@ struct co {
     uint8_t stack[STACK_SIZE];
     void * stack_top;
     //stack should provide room for entry parameters
+    func_t func;
+    void *arg;
     jmp_buf tar_buf;
-    uint8_t alive;
+#define CO_ALIVE 1
+#define CO_RUNNING 2
+    uint8_t stat;
 }routines[MAX_ROUTINES] /*__attribute((aligned(16)))*/,*current;
 
+static void run_co()
 static int pool[MAX_ROUTINES];
 //pool[0,idx) records indexes of
 //available space for routines.
@@ -37,39 +42,48 @@ struct co* new_co(){
         fflush(stderr);
     }
     --idx;
-    routines[pool[idx]].alive=1;
+    routines[pool[idx]].stat=CO_ALIVE;
     return &routines[pool[idx]];
 }
 void co_init() {
     int i;
     for(i=0;i<MAX_ROUTINES;++i){
         pool[i]=i;
-        routines[i].alive=0;
+        routines[i].stat=0;
     }
 }
 
 void *__stack_backup=NULL;
 static jmp_buf ret_buf;
 struct co* co_start(const char *name, func_t func, void *arg) {
-  get_sp(__stack_backup);
-  if(!setjmp(ret_buf)){
-    current=new_co();
-    uint8_t* stack_top=current->stack+STACK_SIZE;
-    stack_top-=sizeof(void*)+((void*)&name)-__stack_backup;
-    //Cal the space for entry parameters
-    *(uintptr_t*)(stack_top+(((void*)&name)-__stack_backup))=(uintptr_t)name;
-    *(uintptr_t*)(stack_top+(((void*)&func)-__stack_backup))=(uintptr_t)func;
-    *(uintptr_t*)(stack_top+(((void*)&arg )-__stack_backup))=(uintptr_t)arg;
-    current->stack_top=stack_top;
-    set_sp(stack_top);
-    func(arg);
-    current->alive=0;
-    longjmp(ret_buf,1);
-  }
-  set_sp(__stack_backup);
+  current=new_co();
+  uint8_t* stack_top=current->stack+STACK_SIZE;
+  stack_top-=sizeof(void*)+((void*)&name)-__stack_backup;
+  //Calculate the space for entry parameters
+#define mov_between(_para) \
+  *(uintptr_t*)(stack_top+(((void*)&_para)-__stack_backup))=(uintptr_t)_para;
+  mov_between(name);
+  mov_between(func);
+  mov_between(arg);
+
+  current->stack_top=stack_top;
+  current->func=func;
+  current->arg=arg;
+  longjmp(ret_buf,1);
   return current;
 }
 
+#define run_co(_co) \
+  current=_co; \
+  if(!(_co->stat&&CO_ALIVE)) \
+    fprintf(stderr,"Run a dead coroutine!\n");fflush(stderr); \
+  if(_co->stat&&CO_RUNNING){ \
+    longjmp(_co->tar_buf,1); \
+  }else{ \
+    _co->stat|=CO_RUNNING; \
+    set_sp(_co->stack_top); \
+    _co->func(_co->arg) \
+  }
 void co_yield() {
     int val=setjmp(current->tar_buf);
     if(val==0){
@@ -80,10 +94,12 @@ void co_yield() {
 }
 
 void co_wait(struct co *thd) {
+  get_sp(__stack_backup);
   setjmp(ret_buf);
   while(thd->alive){
-      current=thd;
-      longjmp(thd->tar_buf,1);
+      run_co(thd);
   }
+  set_sp(__stack_backup);
+  thd->stat=0;
 }
 
