411e15a4d0b65471226988e88bc3571a89790c29
tracer-nju <tracer@nju.edu.cn>
2019-03-16 15:20:05 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: d67bbcf1de0545d2952e2bf0a075cd59   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  15:20:05 up 3 days,  6:02,  7 users,  load average: 0.22, 0.33, 0.45

--- libco/co.c
+++ libco/co.c
@@ -64,9 +64,9 @@ struct co* co_start(const char *name, func_t func, void *arg) {
   get_sp(__stack_backup);
   current=new_co();
   //asm volatile("mov 0x0("SP"),%0;": "=g"(current->args_space[0]):);
-  current->args_space[0]=func;
+  current->args_space[0]=(uintptr_t)func;
   //asm volatile("mov %1("SP"),%0;": "=g"(current->args_space[1]):"n"(sizeof(void*)));
-  current->args_space[1]=arg;
+  current->args_space[1]=(uintptr_t)arg;
   set_sp(current->args_space);
   if(!setjmp(ret_buf)){
       func(arg);
