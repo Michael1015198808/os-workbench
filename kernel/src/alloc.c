b511a42d0b5f9d5b7158765a0c7f87485d9aff73
tracer-nju <tracer@nju.edu.cn>
2019-03-25 09:39:14 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 6430fa49b82e4f80b85b9e1b7ba3d695   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  09:39:14 up 17:07,  4 users,  load average: 0.32, 0.40, 0.43

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -7,13 +7,17 @@ static uintptr_t pm_start, pm_end;
 struct header{
     struct header *next;
     uintptr_t size;
-    uintptr_t space;//directly return &space
+    struct{}space;//space doesn't take any storage
+    //directly return &space
 }*free_list[4]={};
 typedef struct header header;
 
 static void pmm_init() {
   int i,cpu_cnt=_ncpu();
   pm_start = (uintptr_t)_heap.start;
+  pm_start = ~pm_start;
+  pm_start&= !((uintptr_t)0x10);
+  pm_start = ~pm_start;
   pm_end   = (uintptr_t)_heap.end;
   for(i=0;i<cpu_cnt;++i){
       free_list[i]=(void*)pm_start;
@@ -21,7 +25,7 @@ static void pmm_init() {
       free_list[i]->size=0;//Sentinel
       header *head=free_list[i]->next;
       head->next=free_list[i];//Circular
-      head->size=1 KB-sizeof(header)*2+sizeof(uintptr_t);
+      head->size=1 KB-sizeof(header)*2;
       pm_start+=1 KB;
       if(((void*)head)+head->size+sizeof(header)!=(void*)pm_start){
           printf("result%p\nactual%p\n",
@@ -34,26 +38,32 @@ static void pmm_init() {
 static void *kalloc(size_t size) {
   int cpu_id=_cpu();//Call once
   uint8_t *tail=NULL;
-  header *p=free_list[cpu_id],*prevp=NULL,*ret;
-  do{
-    if(p->size>=size){
-      if(p->size-size>16){
-        tail=(uint8_t*)p;
-        tail+=p->size;
-        tail-=size;//Get to the tail
-        ret=(header*)tail;
-        ret->size=size;//record size for free
-        p->size-=size;//Shrink current space
-        return &(ret->space);
-      }else{
-        return NULL;
+  header *p=free_list[cpu_id]->next,*prevp=free_list[cpu_id],*ret;
+  if(size> 1 KB){
+    static pthread_mutex_t kalloc_lock;
+    pthread_mutex_lock(&kalloc_lock);
+    //TODO:Fancy algorithm
+    pthread_mutex_unlock(&kalloc_lock);
+  }else{
+    do{
+      if(p->size>=size){
+        if(p->size-size>sizeof(header)){
+          tail=(uint8_t*)p;
+          tail+=p->size;
+          tail-=size;//Get to the tail
+          ret=(header*)tail;
+          ret->size=size;//record size for free
+          p->size-=size;//Shrink current space
+          return &(ret->space);
+        }else{
+          prevp->next=p->next;//"delete" p
+          return &(p->space);
+        }
       }
-    }
-    prevp=p;
-    p=p->next;
-  }while(p!=free_list[cpu_id]);
-  //TODO: ask os for one more page
-  //return kalloc(size);
+      prevp=p;
+      p=p->next;
+    }while(p!=free_list[cpu_id]);
+  }
   return prevp;//Prevent compile error
 }
 
