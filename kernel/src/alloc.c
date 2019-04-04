3d94d35f13e5aa46a77535305cc2fc1741f4c813
tracer-nju <tracer@nju.edu.cn>
2019-04-04 12:51:22 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 6a447d35b7ad47e8b0a568d379edf679   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  12:51:22 up 15:51,  1 user,  load average: 0.64, 0.57, 0.82

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -56,9 +56,14 @@ static void* big_page_alloc(uintptr_t shift){
         }else if(pages[right]&(1<<shift)){
             idx=right;
         }else{
+#ifdef RETURN_NULL
+            return NULL;
+#else
             printf("No space left!\n");
+            printf("See %s: %d for more info!\n",__FILE__,__LINE__);
+            fflush(stdout)
             while(1);
-            return NULL;//No space
+#endif
         }
     }
     disable(idx,shift);
