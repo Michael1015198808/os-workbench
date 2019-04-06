eefd84dd0857844e7d580e0b521b87754e80401e
tracer-nju <tracer@nju.edu.cn>
2019-04-06 11:12:12 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: d6127a11b6d34720b91dff8173ad0941   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:12:12 up  1:30,  4 users,  load average: 0.17, 0.24, 0.27

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -4,11 +4,12 @@ int main(int argc, char *argv[]) {
   //char s[100]={};
   int pipes[2];
   if(!pipe(pipes)){
-      //int backup=dup(1);
+      int backup=dup(1);
       dup2(pipes[1],1);
       printf("Hello, world!\n");
-      //dup2(backup,1);
-      //printf("Hello, world!\n");
+      fflush(stdout);
+      dup2(backup,1);
+      printf("Hello, world!\n");
       //backup=dup(0);
       //dup2(pipes[0],0);
       //scanf("%s",s);
