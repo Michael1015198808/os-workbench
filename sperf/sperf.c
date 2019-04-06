2c7808b9ca3977c85e228ce1c2b4b744c1b05bf2
tracer-nju <tracer@nju.edu.cn>
2019-04-06 10:01:31 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: d6127a11b6d34720b91dff8173ad0941   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  10:01:31 up 19 min,  3 users,  load average: 0.25, 0.35, 0.33

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,8 +1,15 @@
 #include <unistd.h>
 #include <stdio.h>
 int main(int argc, char *argv[]) {
-  if(fork()==0){
-    printf("Hello\n");
+  char s[100]={};
+  int pipes[2];
+  if(!pipe(pipes)){
+      write(pipes[1],"Hello, world!\n",7);
+      read(pipes[0],s,10);
+      puts(s);
   }
+  /*if(fork()==0){
+    printf("Hello\n");
+  }*/
   return 0;
 }
