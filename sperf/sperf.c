189a630f378b8752bbf251df762e8e4cfadfaaf2
tracer-nju <tracer@nju.edu.cn>
2019-04-06 11:50:54 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: d6127a11b6d34720b91dff8173ad0941   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:50:54 up  2:09,  3 users,  load average: 0.35, 0.36, 0.27

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,37 +1,42 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+void stop(void){
+  fflush(stdout);
+  exit(1);
+}
 int main(int argc, char *argv[],char *envp[]) {
-  //char s[100]={};
-  //int pipes[2];
-  char **new_argv=(char**)malloc(sizeof(void*)*argc);
-  if(new_argv==NULL){
+  char s[100]={};
+  int pipes[2];
+  if(pipe(pipes)){
+    printf("Build pipe failed!\n");
+    stop();
+  }
+  int ret;
+  if(!(ret=fork())){
+    //Child process
+    char **new_argv=(char**)malloc(sizeof(void*)*argc);
+    if(new_argv==NULL){
       printf("No space for new_argv\n");
       fflush(stdout);
       exit(1);
-  }
-  int i;
-  for(i=1;i<=argc;++i){
+    }
+    int i;
+    for(i=1;i<=argc;++i){
       new_argv[i-1]=argv[i];
+    }
+    new_argv[argc]=NULL;
+    printf("argv[1]:%s\n",argv[1]);
+    execve(new_argv[0],new_argv,envp);
+    printf("%s:%d Should not reach here!\n",__FILE__,__LINE__);
+    stop();
+  }else
+    //Parent process
+    if(ret==-1){
+    printf("Fork failed!\n");
+    stop();
+  }else{
+    //Fork success
   }
-  new_argv[argc]=NULL;
-  printf("argv[1]:%s\n",argv[1]);
-  execve(new_argv[0],new_argv,envp);
-  printf("%s:%d Should not reach here!\n",__FILE__,__LINE__);
-  /*if(!pipe(pipes)){
-      int backup=dup(1);
-      dup2(pipes[1],1);
-      printf("Hello, world!\n");
-      fflush(stdout);
-      dup2(backup,1);
-      backup=dup(0);
-      dup2(pipes[0],0);
-      scanf("%s\n",s);
-      dup2(backup,0);
-      puts(s);
-  }*/
-  /*if(fork()==0){
-    printf("Hello\n");
-  }*/
   return 0;
 }
