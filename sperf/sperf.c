87e252c0457e6d941dc7a5a514a3f439d03a2557
tracer-nju <tracer@nju.edu.cn>
2019-04-06 11:27:10 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: d6127a11b6d34720b91dff8173ad0941   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:27:10 up  1:45,  4 users,  load average: 0.22, 0.21, 0.20

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[],char *envp[]) {
   //char s[100]={};
   //int pipes[2];
   char *new_argv[]={NULL,"hello","world",NULL};
-  execve("echo",new_argv,envp);
+  execve("/bin/echo",new_argv,envp);
   printf("%s:%d Should not reach here!\n",__FILE__,__LINE__);
   /*if(!pipe(pipes)){
       int backup=dup(1);
