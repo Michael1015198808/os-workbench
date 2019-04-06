b1c4f15291e547f27b089919050addf339d50e7e
tracer-nju <tracer@nju.edu.cn>
2019-04-06 11:06:51 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: d6127a11b6d34720b91dff8173ad0941   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  11:06:51 up  1:24,  4 users,  load average: 0.29, 0.40, 0.32

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -4,8 +4,14 @@ int main(int argc, char *argv[]) {
   char s[100]={};
   int pipes[2];
   if(!pipe(pipes)){
-      write(pipes[0],"Hello, world!\n",7);
-      read(pipes[1],s,10);
+      int backup=dup(1);
+      dup2(pipes[1],1);
+      printf("Hello, world!\n");
+      dup2(backup,1);
+      int backup=dup(0);
+      dup2(pipes[0],0);
+      scanf("%s",s);
+      dup2(backup,0);
       puts(s);
   }
   /*if(fork()==0){
