a6e52fca99049c1cea20c4351e31aea48eb58c51
tracer-nju <tracer@nju.edu.cn>
2019-04-11 14:53:54 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 38cf2af7f7e2436380e32f40f9fc0420   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  14:53:54 up  6:47,  2 users,  load average: 3.45, 3.94, 4.07

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+char cmd[1000];
+int suffix_of(char *,char *);
 int main(int argc, char *argv[]) {
+  while(true){
+    printf(">> ");
+    fgets(cmd,sizeof(cmd),stdin);
+    if(suffix_of("int",cmd)){
+    }else{
+        char file[]="XXXXXX";
+        int fd=mkstemp(file);
+        printf("%s\n",file);
+        getchar();
+        break;
+    }
+  }
   return 0;
+}
+int suffix_of(char *s1,char *s2){
+    while(*s1&&*s1==*s2)
+        ++s1,++s2;
+    return *s1=='\0';
 }
