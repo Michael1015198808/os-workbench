ec9e0f80b30c6502aa70fb5ecc5d95c12bc687f8
tracer-nju <tracer@nju.edu.cn>
2019-04-12 20:35:04 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 38cf2af7f7e2436380e32f40f9fc0420   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  20:35:05 up 1 day, 12:29,  3 users,  load average: 3.18, 3.37, 3.72

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <dlfcn.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 char cmd[1000];
+char *cflags[]={
+    "-g",
+    "-c",
+};
 int suffix_of(char *,char *);
 int main(int argc, char *argv[]) {
   while(1){
@@ -14,6 +19,11 @@ int main(int argc, char *argv[]) {
     write(fd,cmd,strlen(cmd));
     getchar();
     unlink(file);
+    void *handle;
+    handle=dlopen(file, RTLD_LAZY | RTLD_DEEPBIND);
+    void (*fun)(void)= dlsym(handle, "fun");
+    fun();
+
     /*if(suffix_of("int",cmd)){
     }else{
         break;
