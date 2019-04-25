a33fa88ce4d5f648f2f5e9e593f1230e94bd1be4
tracer-nju <tracer@nju.edu.cn>
2019-04-25 16:05:16 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: bca5201dd8d44647b73f219b02102546   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-8-amd64       Architecture: x86-64  16:05:17 up 1 day,  6:09,  3 users,  load average: 0.46, 0.51, 0.62

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -29,7 +29,7 @@ char *cflags[]={
 };
 int suffix_of(char *,char *);
 int main(int argc, char *argv[],char *envp[]) {
-  /*while(1){
+  while(1){
     //Input
     printf(">> ");
     fgets(cmd,sizeof(cmd),stdin);
@@ -49,19 +49,17 @@ int main(int argc, char *argv[],char *envp[]) {
     getchar();
     unlink(file);
     void *handle;
-    log("%s\n",out);*/
-    void *handle;
-    strcpy(out,"./test.so");
+    log("%s\n",out);
     assert(handle=dlopen(out, RTLD_LAZY|RTLD_GLOBAL));
     int (*fun)(void)= dlsym(handle, "fun");
     assert(fun);
-    printf("function returns %d",fun());
+    printf("function returns %d\n",fun());
 
     /*if(suffix_of("int",cmd)){
     }else{
         break;
     }*/
-  //}
+  }
   return 0;
 }
 int suffix_of(char *s1,char *s2){
