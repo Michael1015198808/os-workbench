3019536b0b9be5a4b7d132b6960a53c02b1ad9f0
tracer-nju <tracer@nju.edu.cn>
2019-05-10 17:56:34 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 1e2d0391da594f16935fb59b16cf2454   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  17:56:34 up  6:52,  3 users,  load average: 1.10, 1.11, 0.84

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -88,7 +88,7 @@ static _Context *os_trap(_Event ev, _Context *context) {
 
 
 static void os_on_irq(int seq, int event, handler_t handler) {
-    Assert(handlers==NULL);
+    Assert(handlers==(void*)NULL);
     irq_handler *prev=NULL,*p=handlers;
 //prev->new->p
     while(p){
