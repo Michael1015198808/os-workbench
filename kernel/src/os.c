c61b0fdfe69d2ded65c72a178f54881b6eb75abb
tracer-nju <tracer@nju.edu.cn>
2019-05-10 18:00:48 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 1e2d0391da594f16935fb59b16cf2454   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  18:00:48 up  6:57,  3 users,  load average: 0.39, 0.76, 0.76

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -84,7 +84,9 @@ static _Context *os_trap(_Event ev, _Context *context) {
 
 
 static void os_on_irq(int seq, int event, handler_t handler) {
-    Assert(handlers==(void*)NULL,"Handler haven't initialized");
+    //Assert(handlers==(void*)NULL,"Handler haven't initialized");
+    log("Handlers:%p\n",handlers);
+    while(1);
     irq_handler *prev=NULL,*p=handlers;
 //prev->new->p
     while(p){
