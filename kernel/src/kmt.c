43e97da9dfbced2fcabb5d4900b70690c6826433
tracer-nju <tracer@nju.edu.cn>
2019-05-13 18:47:28 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 1e2d0391da594f16935fb59b16cf2454   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  18:47:28 up 3 days,  7:43,  4 users,  load average: 0.27, 0.36, 0.45

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -16,6 +16,7 @@ static _Context* kmt_context_switch(_Event ev, _Context *c){
 void kmt_init(void){
     os->on_irq(INT_MIN, _EVENT_NULL, kmt_context_save);
     os->on_irq(INT_MAX, _EVENT_NULL, kmt_context_switch);
+    void irq_test(void);
     irq_test();
 }
 int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *arg){
