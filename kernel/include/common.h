93ac24ea91f6c424685316a05ac5ff641c58d292
tracer-nju <tracer@nju.edu.cn>
2019-05-14 13:35:06 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 6a0e4c3b95e745d89af44d9c46c5be4c   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  13:35:06 up  3:56,  3 users,  load average: 0.47, 0.64, 0.71

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -49,7 +49,7 @@ struct semaphore {
 #define Assert(cond,...) \
     do { \
       if(!(cond)){ \
-        log("\33[1;31mAssertion fails!\33[0m\n",__VA_ARGS__); \
+        log("\33[1;31mAssertion fails!\33[0m\n" __VA_ARGS__); \
         _halt(0); \
       } \
     }while(0)
