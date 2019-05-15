9fa6f2bd1a3dfe09b244fd8ad4929684629e8863
tracer-nju <tracer@nju.edu.cn>
2019-05-15 19:37:34 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: f70a39545fc047f1b8a05b78b0387590   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  19:37:35 up  3:32,  5 users,  load average: 0.95, 1.06, 0.72

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -41,7 +41,7 @@ static void os_init() {
     kmt->create(pmm->alloc(sizeof(task_t)),"echo-test",echo_test,"s");
     kmt->create(pmm->alloc(sizeof(task_t)),"echo-test",echo_test,"l");
     kmt->create(pmm->alloc(sizeof(task_t)),"sem-test",sem_test,NULL);
-    kmt->init(&echo_sem,"echo-sem",2);
+    kmt->sem_init(&echo_sem,"echo-sem",2);
     log("Os init finished\n");
     //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
     //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty2");
