5148ddb62436ca251f26e42a055e4134f1ee77be
tracer-nju <tracer@nju.edu.cn>
2019-05-16 11:16:49 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 5d0e5400c25e4af89c1f603e6af640d1   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  11:16:50 up 11:48,  5 users,  load average: 0.27, 0.38, 0.46

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -205,7 +205,7 @@ static void sem_remove_task(sem_t *sem){
 
 extern pthread_mutex_t irq_lk;
 void kmt_sem_wait(sem_t *sem){
-    kmt->spin_lock(&irq_lk);
+    pthread_mutex_lock(&irq_lk);
     kmt->spin_lock(&(sem->lock));
     --(sem->value);
     if(sem->value>sem->capa){
@@ -214,10 +214,10 @@ void kmt_sem_wait(sem_t *sem){
         return sem_add_task(sem);
     }
     kmt->spin_unlock(&(sem->lock));
-    kmt->spin_unlock(&irq_lk);
+    pthread_mutex_unlock(&irq_lk);
 }
 void kmt_sem_signal(sem_t *sem){
-    kmt->spin_lock(&irq_lk);
+    pthread_mutex_lock(&irq_lk);
     kmt->spin_lock(&(sem->lock));
     ++(sem->value);
     if(sem->value>sem->capa){
@@ -226,7 +226,7 @@ void kmt_sem_signal(sem_t *sem){
         sem_remove_task(sem);
     }
     kmt->spin_unlock(&(sem->lock));
-    kmt->spin_unlock(&irq_lk);
+    pthread_mutex_unlock(&irq_lk);
 }
 MODULE_DEF(kmt) {
   .init        =kmt_init,
