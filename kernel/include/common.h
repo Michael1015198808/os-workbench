d18f165993afb2ddf30367d3e9156ea09210961f
tracer-nju <tracer@nju.edu.cn>
2019-05-18 23:03:13 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ed07937009aa4cbf84c7d2ba0fbc0341   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  23:03:13 up  8:30,  6 users,  load average: 0.45, 0.36, 0.49

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -7,7 +7,7 @@
 #include <limits.h>
 #include <stdint.h>
 
-#define sem_log(A,info) A->idx&=(1<<16)-1;A->idx+=sprintf(A->log+A->idx,"[cpu%d]%s:%s\n",_cpu(),tasks[currents[_cpu()]]->name, #info)
+#define sem_log(A,info,...) A->idx&=(1<<16)-1;A->idx+=sprintf(A->log+A->idx,"[cpu%d]%s:%s %d\n",_cpu(),tasks[currents[_cpu()]]->name,  #info, A->value)
 #define TASK_FENCE
 #define DEBUG
 //Comment the line above after testing
