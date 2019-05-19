9ad7602393e4ea4685a353f8f8c1b0253b7f48cb
tracer-nju <tracer@nju.edu.cn>
2019-05-19 21:26:57 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ed07937009aa4cbf84c7d2ba0fbc0341   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  21:26:58 up 1 day,  6:53,  7 users,  load average: 1.02, 1.25, 1.23

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -220,11 +220,11 @@ void kmt_sem_wait(sem_t *sem){
     kmt->spin_lock(&(sem->lock));
     sem_log(sem,lock);
     --(sem->value);
-    if(sem->value>sem->capa){
+    /*if(sem->value>sem->capa){
         sem_remove_task(sem);
     }else if(sem->value<0){
         return sem_add_task(sem);
-    }
+    }*/
     sem_log(sem,unlock);
     kmt->spin_unlock(&(sem->lock));
 }
