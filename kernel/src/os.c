c260e686dfa0a356ca5c1fce028cda2c44613440
tracer-nju <tracer@nju.edu.cn>
2019-05-19 12:44:50 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ed07937009aa4cbf84c7d2ba0fbc0341   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  12:44:50 up 22:11,  6 users,  load average: 0.41, 0.47, 0.60

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -92,6 +92,7 @@ static _Context *os_trap(_Event ev, _Context *context) {
     irq_idx+=sprintf(irq_log+irq_idx,"[cpu%d]task%dlock\n",_cpu(),currents[_cpu()]);irq_idx&=(1<<16)-1;*/
     _Context *ret = context;
     switch_flag[_cpu()]=0;
+    intr_close();
 
     for(struct irq *handler=irq_guard.next;
         handler!=&irq_guard;
@@ -106,6 +107,7 @@ static _Context *os_trap(_Event ev, _Context *context) {
     /*irq_idx+=sprintf(irq_log+irq_idx,"[cpu%d]task%dunlock\n",_cpu(),currents[_cpu()]);
     pthread_mutex_unlock(&irq_lk);*/
     //log("ret%p\n",ret);
+    intr_open();
     if(ret==NULL){
         log("\n%d\n",switch_flag[_cpu()]);
     };
