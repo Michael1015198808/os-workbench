f8ac353b56faabf140cd89a8866fb22c506d9dea
tracer-nju <tracer@nju.edu.cn>
2019-05-19 14:15:34 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ed07937009aa4cbf84c7d2ba0fbc0341   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  14:15:34 up 23:42,  6 users,  load average: 0.54, 0.74, 0.83

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -7,7 +7,7 @@
 
 static task_t *tasks[20]={};
 //static spinlock_t tasks_lk;
-static pthread_mutex_t tasks_lk;
+static volatile pthread_mutex_t tasks_lk;
 int tasks_idx=0;
 char tasks_log[66000];
 #define trace_pthread_mutex_lock(_lk) \
