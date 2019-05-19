5d8ded926889cc4feb518e22964399f01db65f52
tracer-nju <tracer@nju.edu.cn>
2019-05-19 22:01:45 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ed07937009aa4cbf84c7d2ba0fbc0341   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  22:01:45 up 1 day,  7:28,  7 users,  load average: 0.82, 0.82, 0.87

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -28,7 +28,7 @@ void echo_test(void *arg){
 void sem_test(void *arg){
     while(1){
         //printf("loop\n");
-        printf("!");
+        printf("~");
         kmt->sem_signal(&echo_sem);
         kmt->sem_signal(&echo_sem);
         //while(1);
