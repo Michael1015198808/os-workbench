2bafdbfeaa2b1f649c57efad79b4dbd632b29dbc
tracer-nju <tracer@nju.edu.cn>
2019-05-19 15:01:37 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ed07937009aa4cbf84c7d2ba0fbc0341   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  15:01:37 up 1 day, 28 min,  6 users,  load average: 0.76, 0.82, 0.80

--- kernel/src/cli.c
+++ kernel/src/cli.c
@@ -7,10 +7,8 @@ int intr_idx_[2];
 void intr_close(){
     int cpu_id=_cpu();
     Assert(ncli[cpu_id]>=0);
-    if(ncli[cpu_id]==0){
-        ori[cpu_id]=_intr_read();
-        _intr_write(0);
-    }
+    ori[cpu_id]|=_intr_read();
+    _intr_write(0);
     ++ncli[cpu_id];
 }
 void intr_open(){
@@ -18,6 +16,7 @@ void intr_open(){
     --ncli[cpu_id];
     if(ncli[cpu_id]==0){
         _intr_write(ori[cpu_id]);
+        ori[cpu_id]=0;
     }
     Assert(ncli[cpu_id]>=0);
 }
