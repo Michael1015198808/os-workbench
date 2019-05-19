5c5073ea351e24e8e8eb65c5b60cc047d3f329a8
Yanyan Jiang <jiangyy@outlook.com>
2019-05-19 10:07:45 +0800

    add M6

---
+++ libkvdb/kvdb.h
@@ -0,0 +1,14 @@
+#ifndef __KVDB_H__
+#define __KVDB_H__
+
+
+struct kvdb {
+};
+typedef struct kvdb kvdb_t;
+
+int kvdb_open(kvdb_t *db, const char *filename);
+int kvdb_close(kvdb_t *db);
+int kvdb_put(kvdb_t *db, const char *key, const char *value);
+char *kvdb_get(kvdb_t *db, const char *key);
+
+#endif
