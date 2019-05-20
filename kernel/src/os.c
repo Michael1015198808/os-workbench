7f7b68b654d24a26a30b48e069808a7b6974e923
tracer-nju <tracer@nju.edu.cn>
2019-05-20 15:47:36 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: e4bdf28de52e49d78b87fadb5563c90f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  15:47:36 up  5:15,  5 users,  load average: 0.67, 0.58, 0.49

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -91,7 +91,7 @@ static _Context *os_trap(_Event ev, _Context *context) {
     irq_idx+=sprintf(irq_log+irq_idx,"[cpu%d]task%dlock\n",_cpu(),currents[_cpu()]);irq_idx&=(1<<16)-1;*/
     _Context *ret = context;
     switch_flag[_cpu()]=0;
-    extern task_t *tasks[20];
+    extern task_t *tasks[20];(void)tasks;
     intr_log("close");
     intr_close();
 
