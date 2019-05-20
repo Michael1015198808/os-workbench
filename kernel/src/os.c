ce9abf868d105303cdc1a00492d2d8808ad99dfa
tracer-nju <tracer@nju.edu.cn>
2019-05-20 15:42:18 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: e4bdf28de52e49d78b87fadb5563c90f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  15:42:18 up  5:10,  5 users,  load average: 0.66, 0.46, 0.44

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -71,8 +71,7 @@ static void os_run() {
     /*if(_cpu()==0){
         printf("%d\n",*(int*)0);
     }*/
-    _intr_write(1);
-    while(1);
+    _intr_write(0);
     hello();
     while (1) {
         _yield();
