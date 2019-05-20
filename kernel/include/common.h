40c183c6afa5456bba928d253e1b12e012cef0cd
tracer-nju <tracer@nju.edu.cn>
2019-05-20 15:29:34 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: e4bdf28de52e49d78b87fadb5563c90f   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  15:29:34 up  4:57,  5 users,  load average: 0.66, 0.41, 0.43

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -15,7 +15,7 @@
     #define sem_log(...) 
 #endif
 
-#define intr_log(info) {int cpu_id=_cpu();(intr_idx_+=sprintf(intr_log_+intr_idx_[cpu_id],"\n [cpu%d]%s:%d %s(%d):%s %d",cpu_id,__FILE__,__LINE__,tasks[currents[cpu_id]]->name,currents[cpu_id],info,ncli[cpu_id]));intr_idx_[cpu_id]&=(1<<16)-1;}
+#define intr_log(info) {int cpu_id=_cpu();(intr_idx_+=sprintf(intr_log_+intr_idx_,"\n [cpu%d]%s:%d %s(%d):%s %d",cpu_id,__FILE__,__LINE__,tasks[currents[cpu_id]]->name,currents[cpu_id],info,ncli[cpu_id]));intr_idx_[cpu_id]&=(1<<16)-1;}
 #define detail_log(_log,_idx,info) \
     do{ \
         int cpu_id=_cpu(),_old=_idx; \
