706a15439dd802da55523e7cbe59350a6c3fdcda
tracer-nju <tracer@nju.edu.cn>
2019-05-26 11:07:28 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: b7647f7243894248bef13971c0b62db1   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  11:07:29 up 47 min,  3 users,  load average: 0.44, 0.44, 0.54

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,3 +1,34 @@
+#include <assert.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <regex.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 int main(int argc, char *argv[]) {
-  return 0;
+    int fd = open("./test_file", O_RDWR);
+    struct stat st;
+    fstat(fd, &st);
+    uint8_t *byte_ptr = mmap(NULL, st.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    // read a byte from the file
+    uint8_t byte = byte_ptr[12];
+    for(int i=0;i<15;++i){
+        putchar(byte_ptr[i]);
+    }
+
+    close(fd);
+    return 0;
 }
