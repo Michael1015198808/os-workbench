cff491b3fb74d7f299dcb17213c5054137f12412
tracer-nju <tracer@nju.edu.cn>
2019-05-28 10:37:25 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: b7647f7243894248bef13971c0b62db1   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  10:37:25 up 2 days, 17 min,  6 users,  load average: 1.05, 1.23, 1.25

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -17,36 +17,40 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+
+#define offset_of(member,struct) ((uintptr_t)&(((struct*)0)->member))
+#define pstruct struct __attribute__((packed))
+#define my_cmp(pat, ptr) strncmp(pat,ptr,sizeof(pat))
 typedef union bmp{
     uint8_t info[0];
     
-    struct {
-        struct __attribute__((packed)){
+    pstruct{
+        pstruct {
             uint16_t type;
             uint32_t size;
             uint16_t unused[2];
             uint32_t offset;
         }bfh;
         //Bitmap file header
-        struct{
-            
+        pstruct{
+            uint32_t size;
+            uint16_t width,height,planes,bit_per_pixel;
         }dibh;
         //DIB header
     };
 }bmp_t;
-#define offset_of(member,struct) ((uintptr_t)&(((struct*)0)->member))
-//_Static_assert(offset_of(dibh,bmp_t)==14,"Offset of DIB header is wrong!");
+_Static_assert(offset_of(dibh,bmp_t)==14,"Offset of DIB header is wrong!");
 int main(int argc, char *argv[]) {
     printf("%d\n",(int)offset_of(dibh,bmp_t));
     return 0;
     int fd = open("./fs.img", O_RDONLY);
     struct stat st;
     fstat(fd, &st);
-    uint8_t *image = mmap(NULL, st.st_size, PROT_READ , MAP_SHARED, fd, 0);
+    uint8_t *fs = mmap(NULL, st.st_size, PROT_READ , MAP_SHARED, fd, 0);
 
     // read a byte from the file
-    for(uint64_t i=0;i<st.st_size;++i){
-        putchar(image[i]);
+    for(uint64_t i=0;i<st.st_size;i+=0x100){
+        if(my_cmp("BM",fs+i));
     }
 
     close(fd);
