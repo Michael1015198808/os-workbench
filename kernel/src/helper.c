db696605354b46bd1bff57b0e4a7b649a5964d7e
tracer-nju <tracer@nju.edu.cn>
2019-05-29 13:46:33 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  13:46:33 up  3:17,  7 users,  load average: 0.18, 0.36, 0.38

--- kernel/src/helper.c
+++ kernel/src/helper.c
@@ -1,7 +1,16 @@
 //No lock, so can be called by gdb when dead-locked.
+#include <common.h>
 void _putc(char);
 void print(char* s){
     do{
         _putc(*s);
     }while(*(++s));
+}
+void show_attr(){
+    int i;
+    printf(",%d",tasks[0]->attr);
+    for(i=1;i<tasks_cnt;++i){
+        printf(",%d",tasks[i]->attr);
+    }
+    putchar('\n');
 }
