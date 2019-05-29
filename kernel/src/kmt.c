624cb6cdc2398b66cb785b6f42a8a2af599fafe8
tracer-nju <tracer@nju.edu.cn>
2019-05-29 13:03:29 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  13:03:30 up  2:34,  7 users,  load average: 0.43, 0.37, 0.31

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -101,6 +101,7 @@ int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *a
     }
     //task->id=tasks_cnt;
     log("create (%d)%s\n",tasks_cnt,name);
+    add_task(task);
     Assert(tasks_cnt<LEN(tasks),"%d\n",tasks_cnt);
     task->cpu=-1;
     task->attr=TASK_RUNABLE;
