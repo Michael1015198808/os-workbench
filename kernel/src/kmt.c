d6b2fc76a4cf5bae68d61aa1c318ecee05ee894d
tracer-nju <tracer@nju.edu.cn>
2019-05-30 22:48:30 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  22:48:30 up 1 day, 12:19, 11 users,  load average: 0.70, 0.52, 0.52

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -218,9 +218,15 @@ void kmt_sem_init(sem_t *sem, const char *name, int value){
 
 
 
-int add_rm_cnt=0;
+volatile int add_rm_cnt=0,assert_lock=0;
 static void sem_add_task(sem_t *sem){
+    if(assert_lock==1){
+        printf("Locked!");
+        while(1);
+    }
+    assert_lock=1;
     ++add_rm_cnt;
+    assert_lock=0;
     int cpu_id=_cpu();
     
     sem->pool[sem->tail++]=tasks[current];
@@ -231,7 +237,13 @@ static void sem_add_task(sem_t *sem){
     _yield();
 }
 static void sem_remove_task(sem_t *sem){
+    if(assert_lock==1){
+        printf("Locked!");
+        while(1);
+    }
+    assert_lock=1;
     --add_rm_cnt;
+    assert_lock=0;
 
     neg_flag(sem->pool[sem->head],TASK_SLEEP);
     neg_flag(sem->pool[sem->head],TASK_SLEEP);
