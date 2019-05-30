1742d0e6b052994db091eac317519f70a3164834
tracer-nju <tracer@nju.edu.cn>
2019-05-30 13:12:18 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  13:12:18 up 1 day,  2:43, 11 users,  load average: 0.88, 0.36, 0.29

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -84,7 +84,7 @@ static _Context* kmt_context_switch(_Event ev, _Context *c){
     }while(tasks[new]->attr);
 
     tasks[current]->cpu=-1;
-    //asm volatile("mov $0xfffffffd,0x4(%%rcx)");
+    asm volatile("mov $0xfffffffd,0x4(%%ecx)");
     neg_flag(tasks[current]->attr,TASK_RUNNING);
 
     tasks[new]->cpu=cpu_id;
