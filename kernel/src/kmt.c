9690b3abd77b8718755ed02f277687b3b12c760e
tracer-nju <tracer@nju.edu.cn>
2019-05-30 14:47:55 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  14:47:55 up 1 day,  4:18, 11 users,  load average: 0.50, 0.57, 0.56

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -66,7 +66,11 @@ static void add_task(task_t *task){
 static _Context* kmt_context_save(_Event ev, _Context *c){
     //trace_pthread_mutex_lock(&tasks_lk);
     int cpu_id=_cpu();
-    if(current!=-1){
+    if(current==-1){
+        current=tasks_cnt-1;
+        //kmt->create(pmm->alloc(sizeof(task_t)),"os_run",NULL,NULL);
+        //current=tasks_cnt-1;
+    }else{
         tasks[current]->context=*c;
     }
     //trace_pthread_mutex_unlock(&tasks_lk);
@@ -91,10 +95,8 @@ static _Context* kmt_context_switch(_Event ev, _Context *c){
         }
     }while(tasks[new]->attr);
 
-    if(current!=-1){
-        tasks[current]->cpu=-1;
-        neg_flag(tasks[current]->attr,TASK_RUNNING);
-    }
+    tasks[current]->cpu=-1;
+    neg_flag(tasks[current]->attr,TASK_RUNNING);
 
     tasks[new]->cpu=cpu_id;
     set_flag(tasks[new]->attr,TASK_RUNNING);
