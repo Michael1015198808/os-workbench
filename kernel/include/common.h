2239bb2aed21ae741e2da158557edb74608d7d16
tracer-nju <tracer@nju.edu.cn>
2019-05-30 10:43:31 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  10:43:31 up 1 day, 14 min, 11 users,  load average: 0.44, 0.40, 0.43

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -10,8 +10,10 @@
 #include <my_trace.h>
 
 //#define sem_log(A,info,...) A->idx&=(1<<16)-1;A->idx+=sprintf(A->log+A->idx,"\n[cpu%d]%s:%d %s:%s %d",_cpu(),__func__,__LINE__,tasks[currents[_cpu()]]->name,  #info, A->value)
-#define set_flag(A,B) (A)|=(B)
-#define neg_flag(A,B) (A)&=~(B)
+#define set_flag(A,B) \
+    asm volatile("or %0, %1": "g"(B):"=g"(A))
+#define neg_flag(A,B) \
+    asm volatile("add %0, %1": "g"(B):"=g"(A))
 
 #define LEN(arr) ((sizeof(arr) / sizeof(arr[0])))
 
