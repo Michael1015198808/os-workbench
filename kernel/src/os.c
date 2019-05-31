f291c610cb51210fa832cc7650c713cc57849c9f
tracer-nju <tracer@nju.edu.cn>
2019-05-31 18:32:50 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  18:32:50 up 2 days,  8:03, 10 users,  load average: 1.17, 0.92, 0.76

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -45,6 +45,8 @@ void sem_test(void *arg){
         _yield();
     }
 }
+
+ssize_t tty_write(device_t *dev, off_t offset, const void *buf, size_t count);
 void echo_task(void *name) {
   device_t *tty = dev_lookup(name);
   while (1) {
