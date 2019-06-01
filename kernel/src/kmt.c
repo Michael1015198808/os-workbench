ea4219c33add534c3a00e033dc8f6aae33f6c6ed
tracer-nju <tracer@nju.edu.cn>
2019-06-01 18:18:51 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  18:18:51 up 3 days,  7:49, 11 users,  load average: 0.29, 0.39, 0.50

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -73,10 +73,10 @@ static int add_task(task_t *task){
 static _Context* kmt_context_save(_Event ev, _Context *c){
     int cpu_id=_cpu();
     if(current==-1){
-        kmt->spin_lock(tasks_lk);
+        kmt->spin_lock(&tasks_lk);
         current=kmt->create(pmm->alloc(sizeof(task_t)),"os_run",os->run,NULL);
         tasks[current]->attr|=TASK_RUNNING;
-        kmt->spin_lock(tasks_lk);
+        kmt->spin_lock(&tasks_lk);
     }
     Assert(current>=0);
     tasks[current]->context=*c;
