b7370cd207f2a763c3857075ce26864236809eef
tracer-nju <tracer@nju.edu.cn>
2019-06-01 23:36:02 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  23:36:02 up 3 days, 13:06, 10 users,  load average: 0.38, 0.46, 0.47

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -169,6 +169,8 @@ void kmt_spin_lock(spinlock_t *lk){
                 ++lk->reen;
                 break;
             }else{
+                while(lk->locked);
+                /*
                 for(volatile int i=0;lk->locked;++i){
                     if(i==10000){
                         i=0;
@@ -177,6 +179,7 @@ void kmt_spin_lock(spinlock_t *lk){
                         intr_close();
                     }
                 }
+                */
             }
         }
         pthread_mutex_lock(&lk->locked);
