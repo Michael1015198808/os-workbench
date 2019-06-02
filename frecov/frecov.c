ac1c75a9417a6f151f4253bf0f3a00a60db43575
tracer-nju <tracer@nju.edu.cn>
2019-06-03 00:30:02 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  00:30:02 up 4 days, 14:00,  4 users,  load average: 0.65, 0.86, 0.67

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -257,8 +257,8 @@ outer:;
 #define squ(x) ((x)*(x))
 #define round(x) (x>1600?1600:(x))
                             for(int i=0;i<bmp->dibh.width*3;++i){
-                                if(cnt==-190)
-                                printf("%x,%x",current[i],current[i-width_bytes]);
+                                if(cnt==-189||cnt==-190)
+                                printf("%x,%x\n",current[i],current[i-width_bytes]);
                                 diff+=round(squ(abs(current[i]+current[i-width_bytes*2]-2*current[i-width_bytes])))+
                                       round(squ(abs(current[i]-current[i+1-width_bytes])))+
                                       round(squ(abs(current[i]-current[i-1-width_bytes])));
