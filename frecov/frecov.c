9e390c6567b2a64a3b28cc5b08d58d6ffd1cd211
tracer-nju <tracer@nju.edu.cn>
2019-06-02 16:16:13 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  16:16:14 up 4 days,  5:47,  7 users,  load average: 0.24, 0.31, 0.36

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -125,7 +125,7 @@ int main(int argc, char *argv[]) {
                 fs->bytes_per_sector);
     void *begin=((void*)e)-(2*fs->sectors_per_cluster*fs->bytes_per_sector);
     //printf("%llx\n",1LL*(begin-disk));
-    while((uintptr_t)e<(uintptr_t)end){
+    while((uintptr_t)e!=(uintptr_t)end){
         if(e->attr==0xf){
             long_entry_t *tmp=(void*)e;
             int idx=0;
