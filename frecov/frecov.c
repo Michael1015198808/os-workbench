9edf6d757c2037765d8f12efb779d7292cfa0445
tracer-nju <tracer@nju.edu.cn>
2019-06-02 11:47:27 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  11:47:27 up 4 days,  1:18,  5 users,  load average: 0.36, 0.35, 0.35

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -160,6 +160,7 @@ outer:;
             puts(file_name);
             printf("e:%p\n",e);
             printf("high:%x low:%x\n",e->clus_high,e->clus_low);
+            printf("%llx %x\n",((e->clus_high*1LL<<32)+e->clus_low),fs->bytes_per_sector);
             uint8_t* file=begin+((e->clus_high*1LL<<32)+e->clus_low)*fs->bytes_per_sector;
             printf("%llx",((uintptr_t)file-(uintptr_t)fs)+0xbLL);
             for(uint32_t i=0;i<e->size;++i){
