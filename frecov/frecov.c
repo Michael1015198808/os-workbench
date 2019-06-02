997ed21455df19b1451d8fcffbc5718386e4c5f9
tracer-nju <tracer@nju.edu.cn>
2019-06-03 00:10:14 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  00:10:14 up 4 days, 13:41,  4 users,  load average: 0.35, 0.42, 0.49

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -257,7 +257,7 @@ outer:;
                             for(int i=0;i<bmp->dibh.width*3;i+=4){
                                 diff+=squ(abs(current[i]-current[i-width_bytes]));
                             }
-                            printf("%d\n",diff/bmp->dibh.width);
+                            printf("(%d)%d\n",cnt,diff/bmp->dibh.width);
                             if(diff/bmp->dibh.width>10000){
                                 uint8_t *find=(uint8_t*)(uintptr_t)(disk+
                                             ( fs->sectors_reserved+
