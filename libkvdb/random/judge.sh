4b76656921ec970a11f62bed05fd84e0e88a752d
鄢振宇 <171240518@smail.nju.edu.cn>
2019-06-08 21:02:04 +0800

    Fix judge.sh

--- libkvdb/random/judge.sh
+++ libkvdb/random/judge.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ./gen.out 1> in.c 2>out2
 gcc -g -DDB_FILE=\"db/rand.db\" -I.. -L.. -m64 in.c -o test-64 -lkvdb-64
+gcc -g -DDB_FILE=\"db/rand.db\" -I.. -L.. -m32 in.c -o test-32 -lkvdb-32
 LD_LIBRARY_PATH=.. ./test-64 >out1
 diff out1 out2
 LD_LIBRARY_PATH=.. ./test-32 >out1
