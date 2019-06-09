3322f51ea83763152bca84db9747d5994704e1f6
tracer-nju <tracer@nju.edu.cn>
2019-06-09 15:03:01 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  15:03:02 up 11 days,  4:33,  9 users,  load average: 0.26, 0.37, 0.61

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -153,7 +153,13 @@ uint32_t alloc_str(const char* src,int fd){
         len-=BLOCK_LEN;
         if(flag){
             read(fd,&cur,sizeof(uint32_t));
+            if(len<0){
+                lseek(fd,offsetof(header,backup_next),SEEK_SET);
+                write(fd,&prev,sizeof(uint32_t));
+            }
             if(cur==list[1]){
+                lseek(fd,0,SEEK_SET);
+                write(fd,&list[1],sizeof(uint32_t));
                 cur=lseek(fd,0,SEEK_END)-HEADER_LEN;
                 flag=0;
             }
@@ -163,10 +169,10 @@ uint32_t alloc_str(const char* src,int fd){
         if(len>0){
             write_db(fd, prev+BLOCK_LEN,&cur,sizeof(uint32_t));
         }else{
-            lseek(fd,0,SEEK_SET);
-            write(fd,&cur,sizeof(uint32_t));
-            lseek(fd,offsetof(header,backup_next),SEEK_SET);
-            write(fd,&prev,sizeof(uint32_t));
+            if(flag){
+                lseek(fd,0,SEEK_SET);
+                write(fd,&cur,sizeof(uint32_t));
+            }
             write_db(fd, prev+BLOCK_LEN,zeros,sizeof(uint32_t));
         }
         prev=cur;
