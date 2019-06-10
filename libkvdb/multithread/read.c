6dbcdf7d17a9cd349eb343ec115195cabe716346
tracer-nju <tracer@nju.edu.cn>
2019-06-10 20:31:18 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  20:31:18 up 12 days, 10:01,  9 users,  load average: 0.38, 0.49, 0.46

---
+++ libkvdb/multithread/read.c
@@ -0,0 +1,51 @@
+#include <assert.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <regex.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "kvdb.h"
+
+char *s[]={
+    "1",
+    "26",
+    "51",
+    "76",
+};
+#define LEN_S sizeof(s)/sizeof(s[0])
+int main(){
+    int i=0;
+    char *values[LEN_S];
+    kvdb_t first;
+    kvdb_open(&first,DB_FILE);
+    while(1){
+        for(int i=0;i<LEN_S;++i){
+            values[i]=kvdb_get(&first,s[i]);
+        }
+        //To make output flexible
+        for(int i=0;i<LEN_S;++i){
+            printf("%s: ",s[i]);
+            puts(values[i]);
+        }
+        for(int i=0;i<LEN_S;++i){
+            free(values[i]);
+        }
+        usleep(10000);
+    }
+    kvdb_close(&first);
+    return 0;
+}
