57eceacced8e0c9f2efb5c88101f9bf703dbc92f
tracer-nju <tracer@nju.edu.cn>
2019-06-10 22:49:38 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  22:49:38 up 12 days, 12:20,  8 users,  load average: 0.41, 4.24, 7.72

---
+++ libkvdb/multithread/multi_read_2.c
@@ -0,0 +1,64 @@
+#include "kvdb.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+void *special_test(void *arg){
+    void **args=arg;
+    kvdb_t *db= args[0];
+    int cnt=0;
+    while(1){
+        char key[5],val[20];
+        sprintf(key,"%d",cnt%100);
+        sprintf(val,"%d",cnt);
+        kvdb_put(db,key,val);
+    }
+}
+void *test(void *arg) {
+    void **args=arg;
+    kvdb_t *db = args[0];
+    uintptr_t key_val = (uintptr_t)args[1];
+    char key_str[5],*val,check[5];
+    for(int i=1;i!=0;++i){
+        sprintf(key_str,"%d",key_val);
+        sprintf(check,"%d",key_val/10);
+        val=kvdb_get(db,key_str);
+        if(val==NULL || strcmp(check,val)){
+            asm volatile("int $3");
+        }
+        free(val);
+        usleep(rand()&(0xff));
+        ++key_val;
+        if(key_val==100)key_val=0;
+    }
+    return NULL;
+}
+
+#define THREADS 32
+#define ARGS 2
+#define panic(fmt,...) \
+    fprintf(stderr, __FILE__ ":%d " fmt,__LINE__, ##__VA_ARGS__)
+int main(int argc, char *argv[]) {
+    kvdb_t *db = malloc(sizeof(kvdb_t));
+    uintptr_t args[THREADS][ARGS];
+
+    if(db == NULL) { panic("malloc failed. \n"); return 1; }
+ 
+    if(kvdb_open(db, DB_FILE)) { panic("cannot open. \n"); return 1; }
+ 
+    pthread_t pt[THREADS];
+    for(int i = 0; i < THREADS; i ++) {
+        args[i][0]=(uintptr_t)db;
+        args[i][1]=(100/THREADS)*i+1;
+        pthread_create(&pt[i], NULL, test, args[i]);
+    }
+
+    for(int i = 0; i < THREADS; i ++) {
+        pthread_join(pt[i], NULL);
+    }
+
+    if(kvdb_close(db)) { panic("cannot close. \n"); return 1; }
+    system("systemctl hibernate");
+
+    return 0;
+}
