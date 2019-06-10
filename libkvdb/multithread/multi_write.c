8c7186819fe00bdd02a7ce6f5855aed2f817abfc
tracer-nju <tracer@nju.edu.cn>
2019-06-10 22:38:05 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  22:38:05 up 12 days, 12:08,  8 users,  load average: 22.13, 23.05, 13.14

---
+++ libkvdb/multithread/multi_write.c
@@ -0,0 +1,56 @@
+#include "kvdb.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+void *special_test(void *arg){
+    void **args=arg;
+    kvdb_t *db= args[0];
+    int cnt=0;
+    while(1){
+        char key[5],val[20];
+        sprintf(key,"%d",cnt%100);
+        sprintf(val,"%d",cnt);
+        kvdb_put(db,key,val);
+    }
+}
+void *test(void *arg) {
+    void **args=arg;
+    kvdb_t *db = args[0];
+    uintptr_t idx = (uintptr_t)args[1];
+    for(int i=1;i<11;++i){
+        char key[5],val[20];
+        sprintf(key,"%d",idx+i);
+        sprintf(val,"%d",idx/10);
+        kvdb_put(db,key,val);
+    }
+    return NULL;
+}
+
+#define THREADS 10
+#define ARGS 2
+#define panic(fmt,...) \
+    fprintf(stderr, __FILE__ ":%d " fmt,__LINE__, ##__VA_ARGS__)
+int main(int argc, char *argv[]) {
+    kvdb_t *db = malloc(sizeof(kvdb_t));
+    uintptr_t args[THREADS][ARGS];
+
+    if(db == NULL) { panic("malloc failed. \n"); return 1; }
+ 
+    if(kvdb_open(db, DB_FILE)) { panic("cannot open. \n"); return 1; }
+ 
+    pthread_t pt[THREADS];
+    for(int i = 0; i < THREADS; i ++) {
+        args[i][0]=(uintptr_t)db;
+        args[i][1]=10*i;
+        pthread_create(&pt[i], NULL, test, args[i]);
+    }
+
+    for(int i = 0; i < THREADS; i ++) {
+        pthread_join(pt[i], NULL);
+    }
+
+    if(kvdb_close(db)) { panic("cannot close. \n"); return 1; }
+
+    return 0;
+}
