bf7ade8c313b276398d0858dcece53a057aa37a0
tracer-nju <tracer@nju.edu.cn>
2019-06-10 20:28:29 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: ce96350e6f9d444a8d4733ef133f6aa7   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  20:28:30 up 12 days,  9:58,  9 users,  load average: 0.47, 0.54, 0.48

--- libkvdb/multithread/multi.c
+++ libkvdb/multithread/multi.c
@@ -17,12 +17,12 @@ void *special_test(void *arg){
 void *test(void *arg) {
     void **args=arg;
     kvdb_t *db = args[0];
-    uintptr_t mode = (uintptr_t)args[1];
+    uintptr_t idx = 1000000*(uintptr_t)args[1];
     uintptr_t cnt = (uintptr_t)args[2];
     while(1){
         char key[5],val[20];
         sprintf(key,"%d",cnt%100);
-        sprintf(val,"%d",1000000+cnt);
+        sprintf(val,"%d",idx+cnt);
         kvdb_put(db,key,val);
         ++cnt;
         if(cnt>=1000000)cnt=0;
@@ -32,7 +32,7 @@ void *test(void *arg) {
 }
 
 #define THREADS 4
-#define ARGS 2
+#define ARGS 3
 #define panic(fmt,...) \
     fprintf(stderr, __FILE__ ":%d " fmt,__LINE__, ##__VA_ARGS__)
 int main(int argc, char *argv[]) {
@@ -46,7 +46,7 @@ int main(int argc, char *argv[]) {
     pthread_t pt[THREADS];
     for(int i = 0; i < THREADS; i ++) {
         args[i][0]=(uintptr_t)db;
-        args[i][1]=i&1;
+        args[i][1]=i+1;
         args[i][2]=rand();
         pthread_create(&pt[i], NULL, test, args[i]);
     }
