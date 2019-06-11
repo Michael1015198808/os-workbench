3d946a28ca313206bad8986297499bd68abed91b
tracer-nju <tracer@nju.edu.cn>
2019-06-11 21:40:20 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: 89fbb266e27249ebb31f4a9856391b6c   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  21:40:21 up 0 min,  3 users,  load average: 0.49, 0.16, 0.06

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -85,7 +85,7 @@ __thread int errid;
 
 #define safe_call(call,cond) \
             ( \
-                ret=call, \
+                (ret=call), \
                 ret cond?   \
                 ret: \
                 ( \
@@ -212,7 +212,7 @@ uint32_t alloc_str(const char* src,int fd){
     while(len>0){
         if(len<BLOCK_LEN){
             pwrite(fd,zeros,BLOCK_LEN,cur+HEADER_LEN);
-            pwrite(fd,src  ,len      ,cur+HEADER_LEN);
+            safe_call(pwrite(fd,src  ,len      ,cur+HEADER_LEN),==len);
         }else{
             safe_call(pwrite(fd,src  ,BLOCK_LEN,cur+HEADER_LEN),==BLOCK_LEN);
         }
