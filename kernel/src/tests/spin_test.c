f314b977277a5d6daf30940a9b4e54fc9f9b1b00
tracer-nju <tracer@nju.edu.cn>
2019-06-13 17:35:41 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: a141c45b85a54d03bcc383c554fb9af1   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  17:35:41 up 22:16,  4 users,  load average: 0.20, 0.23, 0.41

--- kernel/src/tests/spin_test.c
+++ kernel/src/tests/spin_test.c
@@ -1,6 +1,16 @@
 #include <devices.h>
 #include <common.h>
 #include <klib.h>
+static inline void fill(uint8_t *p,int a,int b,int len){
+    for(int i=0;i<len;++i){
+        p[i]=(a+=b);
+    }
+}
+static inline void check(uint8_t *p,int a,int b,long long len){
+    for(int i=0;i<len;++i){
+        Assert(p[i]==(uint8_t)(a+=b));
+    }
+}
 void spin_test(){
     static spinlock_t test_spin;
     static int i=0;
