8d2e78f326287afd8a713ca6b66a4d3c2f99253d
tracer-nju <tracer@nju.edu.cn>
2019-06-14 20:56:42 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: a141c45b85a54d03bcc383c554fb9af1   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  20:56:42 up 2 days,  1:37,  5 users,  load average: 0.83, 0.76, 0.64

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -53,22 +53,22 @@ static int add_task(task_t *task){
 
 #define set_flag(A,B) \
     { \
-        report_if(!(A->attr&B),"%d",A->attr); \
+        report_if(!(A->attr&B)); \
         pthread_mutex_lock(&A->attr_lock); \
         uintptr_t p=(uintptr_t)&A->attr; \
         asm volatile("lock or %1,(%0)"::"r"(p),"g"((B))); \
         pthread_mutex_unlock(&A->attr_lock); \
-        report_if(A->attr&B,"%d",A->attr); \
+        report_if(A->attr&B); \
     }
 
 #define neg_flag(A,B) \
     { \
-        report_if(A->attr&B,"%d",A->attr); \
+        report_if(A->attr&B); \
         pthread_mutex_lock(&A->attr_lock); \
         uintptr_t p=(uintptr_t)&A->attr; \
         asm volatile("lock and %1,(%0)"::"r"(p),"g"(~(B))); \
         pthread_mutex_unlock(&A->attr_lock); \
-        report_if(!(A->attr&B),"%d",A->attr); \
+        report_if(!(A->attr&B)); \
     }
 
 static _Context* kmt_context_save(_Event ev, _Context *c){
