15d7d6a8a375e0beb903270074b43861be5616a5
tracer-nju <tracer@nju.edu.cn>
2019-06-14 14:08:25 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: a141c45b85a54d03bcc383c554fb9af1   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  14:08:25 up 1 day, 18:49,  4 users,  load average: 0.80, 0.61, 0.57

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -84,9 +84,8 @@ static void os_run() {
 }
 
 static _Context *os_trap(_Event ev, _Context *context) {
-    if(_cpu()<0){printf("%d\n",ncli[_cpu()]);}
     intr_close();
-    Assert(ncli[_cpu()]==1,"%d",ncli[_cpu()]);
+    repoft_if(ncli[_cpu()]!=1);
     _Context *ret = context;
 
     for(struct irq *handler=irq_guard.next;
