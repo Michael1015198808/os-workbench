b9d4d6a939c3e353bb1e046558e9d00d3c7dde6b
tracer-nju <tracer@nju.edu.cn>
2019-06-14 21:48:24 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: a141c45b85a54d03bcc383c554fb9af1   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  21:48:24 up 2 days,  2:29,  5 users,  load average: 0.65, 0.66, 0.62

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -48,7 +48,7 @@ void echo_task(void *name) {
   device_t *tty = dev_lookup(name);
   while (1) {
     char line[128], text[128];
-    sprintf(text, "(%s) $ ", name); tty_write(tty, text);
+    sprintf(text, "(%s) $ ", name); tty->ops->write(tty, text);
     int nread = tty->ops->read(tty, 0, line, sizeof(line));
     line[nread - 1] = '\0';
     sprintf(text, "Echo: %s.\n", line); tty_write(tty, text);
