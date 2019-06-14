091a42c89b0bae078361b7363d5b1db61a61851f
tracer-nju <tracer@nju.edu.cn>
2019-06-14 18:16:20 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: ff24440e96564ee6b0820289c5c340a9            Boot ID: a141c45b85a54d03bcc383c554fb9af1   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-9-amd64       Architecture: x86-64  18:16:20 up 1 day, 22:57,  5 users,  load average: 0.50, 0.61, 0.67

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -90,11 +90,13 @@ static _Context* kmt_context_switch(_Event ev, _Context *c){
         ++cnt;
         new%=tasks_cnt;
         if(cnt==0){
+            /*
             report_if(1);
             kmt->spin_unlock(&tasks_lk);
             if((tasks[current]->attr&TASK_SLEEP)==0)return &tasks[current]->context;
             for(volatile uint32_t sleep=1;sleep<10000000;++sleep);//Sleep if can't get any process to run
             kmt->spin_lock(&tasks_lk);
+            */
         }
     }while(tasks[new]->attr);
 
