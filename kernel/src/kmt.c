89be101ff08ef6b4f4134079ecd4837326ae93d4
tracer-nju <tracer@nju.edu.cn>
2019-07-01 00:11:55 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 10f69da143d5405ea1a6bd397f87d372   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-19-generic       Architecture: x86-64  00:11:55 up  1:28,  4 users,  load average: 1.11, 0.91, 0.92

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -116,7 +116,6 @@ static _Context* kmt_context_switch(_Event ev, _Context *c){
     kmt->spin_unlock(&tasks_lk);
     for(int i=0;i<4;++i){
         if(tasks[current]->fence1[i]!=0x13579ace||tasks[current]->fence2[i]!=0xeca97531){
-            asm volatile("");
             log("Stack over/under flow!\n");
             while(1);
         }
--- kernel/src/tests/semaphore_test.c
+++ kernel/src/tests/semaphore_test.c
@@ -3,22 +3,22 @@
 #include <klib.h>
 
 semaphore_t test_sem[2];
-//static volatile int to_run=1;
+static volatile int to_run=1;
 void semaphore_test_init(void){
-    kmt->sem_init(test_sem,"test sem:0",0);
+    kmt->sem_init(&test_sem[0],"test sem:0",0);
     kmt->sem_init(&test_sem[1],"test sem:1",1);
 }
 void semaphore_test(void *arg){
     char c=((char*)arg)[0];
     int idx=c-'a';
     while(1){
-        //while(to_run!=idx);
         kmt->sem_wait(  &test_sem[idx]);
+        while(to_run!=idx);
         _putc(c);
         for(int i=0;i<7;++i){
             _putc("Hello!\n"[i]);
         }
         kmt->sem_signal(&test_sem[1-idx]);
-        //to_run^=1;
+        to_run^=1;
     }
 }
