50de344aea6f9e0297343a21b127af94f2c76c31
tracer-nju <tracer@nju.edu.cn>
2019-07-01 00:22:56 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 10f69da143d5405ea1a6bd397f87d372   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-19-generic       Architecture: x86-64  00:22:57 up  1:39,  4 users,  load average: 0.62, 0.98, 1.01

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -231,6 +231,7 @@ static void sem_add_task(sem_t *sem){
     if(sem->tail>=POOL_LEN)sem->tail-=POOL_LEN;
 
     pthread_mutex_unlock(&(sem->lock));
+    intr_open();
     _yield();
 }
 
@@ -242,31 +243,31 @@ static void sem_remove_task(sem_t *sem){
 }
 
 void kmt_sem_wait_real(sem_t *sem){
+    intr_close();
     pthread_mutex_lock(&(sem->lock));
 
     if(--sem->value<0){
         return sem_add_task(sem);
     }
     pthread_mutex_unlock(&(sem->lock));
+    intr_open();
 }
 void kmt_sem_wait(sem_t *sem){
-    intr_close();
     kmt_sem_wait_real(sem);
-    intr_open();
 }
 
 void kmt_sem_signal_real(sem_t *sem){
+    intr_close();
     pthread_mutex_lock(&(sem->lock));
 
     if(++sem->value<=0){
         sem_remove_task(sem);
     }
     pthread_mutex_unlock(&(sem->lock));
+    intr_open();
 }
 void kmt_sem_signal(sem_t *sem){
-    intr_close();
     kmt_sem_signal_real(sem);
-    intr_open();
 }
 
 MODULE_DEF(kmt) {
