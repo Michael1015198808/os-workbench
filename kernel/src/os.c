849ae80ffbd78c8bdddc266401e15dbd9b1605ae
tracer-nju <tracer@nju.edu.cn>
2019-06-30 14:05:00 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: aab9d85012cd44b8ba4b11a2f6fd9cd2   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-19-generic       Architecture: x86-64  14:05:00 up  1:52,  6 users,  load average: 0.79, 1.45, 2.49

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -54,7 +54,7 @@ static void os_init() {
     kmt->init();
     dev->init();
 #define CURRENT_TEST printf_test
-#define TEST_NAME(idx) #CURRENT_TEST #idx
+#define TEST_NAME(idx) "CURRENT_TEST" #idx
     kmt->create(pmm->alloc(sizeof(task_t)),TEST_NAME(1),CURRENT_TEST,"a");
     kmt->create(pmm->alloc(sizeof(task_t)),TEST_NAME(2),CURRENT_TEST,"b");
     kmt->create(pmm->alloc(sizeof(task_t)),TEST_NAME(3),CURRENT_TEST,"c");
