10ee7ff560947aff1f4bd13f892ba0920facfe9e
tracer-nju <tracer@nju.edu.cn>
2019-07-01 13:58:07 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: fa1d2a945032465c926be6e39230d283   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-19-generic       Architecture: x86-64  13:58:07 up  4:50,  5 users,  load average: 3.47, 4.91, 4.26

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -90,7 +90,9 @@ static void hello() {
 static void os_run() {
     hello();
     _intr_write(1);
-    while(1);
+    while(1){
+        _yield();
+    }
 }
 
 static _Context *os_trap(_Event ev, _Context *context) {
