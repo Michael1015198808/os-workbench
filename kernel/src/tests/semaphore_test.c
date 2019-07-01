1ff6e009da5ee6c5c2bc36228ebf5924219bdea9
tracer-nju <tracer@nju.edu.cn>
2019-07-01 09:54:55 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: fa1d2a945032465c926be6e39230d283   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-19-generic       Architecture: x86-64  09:54:55 up 47 min,  4 users,  load average: 1.39, 0.98, 0.89

--- kernel/src/tests/semaphore_test.c
+++ kernel/src/tests/semaphore_test.c
@@ -14,7 +14,7 @@ void semaphore_test(void *arg){
     while(1){
         //while(to_run!=idx);
         kmt->sem_wait(  &test_sem[idx]);
-        printf("%d%cHello!\n",_cpu(),c);
+        printf("%d%cHello!\n",_intr_read(),c);
         //to_run^=1;
         kmt->sem_signal(&test_sem[1-idx]);
     }
