86ebf4886edeffb31b32dd1b102865dfd3bdb0d0
tracer-nju <tracer@nju.edu.cn>
2019-07-01 13:29:33 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: fa1d2a945032465c926be6e39230d283   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-19-generic       Architecture: x86-64  13:29:34 up  4:22,  5 users,  load average: 0.53, 0.73, 0.97

--- kernel/include/test.h
+++ kernel/include/test.h
@@ -1,9 +1,10 @@
 #ifndef NO_TEST
 
 typedef void (*test_func)(void*);
-test_func memory_test;
+void (memory_test)(void*);
+test_func p=memory_test;
 test_func printf_test;
-//test_func spin_test;
+test_func spin_test;
 test_func semaphore_test;
 test_func multithread_test;
 test_func fib_test;
