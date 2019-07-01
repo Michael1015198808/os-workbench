b6c6032e6fb2205acbe4551a24824e8822bc867f
tracer-nju <tracer@nju.edu.cn>
2019-07-01 16:09:29 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: fa1d2a945032465c926be6e39230d283   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-19-generic       Architecture: x86-64  16:09:30 up  7:02,  5 users,  load average: 1.02, 1.37, 1.06

--- kernel/src/tests/printf_test.c
+++ kernel/src/tests/printf_test.c
@@ -7,6 +7,7 @@ void printf_test_init(void){
     kmt->create(pmm->alloc(sizeof(task_t)),"printf_test2",printf_test,"b");
     kmt->create(pmm->alloc(sizeof(task_t)),"printf_test3",printf_test,"c");
     kmt->create(pmm->alloc(sizeof(task_t)),"printf_test4",printf_test,"d");
+    kmt->create(pmm->alloc(sizeof(task_t)),"printf_test5",printf_test,"e");
 }
 void printf_test(void *dummy){
     while(1){
