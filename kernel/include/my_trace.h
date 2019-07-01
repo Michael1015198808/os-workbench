e414c3742a7f834daaddafac5db29bba771c8cf8
tracer-nju <tracer@nju.edu.cn>
2019-07-01 16:44:37 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: fa1d2a945032465c926be6e39230d283   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-19-generic       Architecture: x86-64  16:44:37 up  7:37,  5 users,  load average: 0.58, 0.94, 1.70

--- kernel/include/my_trace.h
+++ kernel/include/my_trace.h
@@ -11,11 +11,8 @@
     //#define intr_log(info) {int cpu_id=_cpu();pthread_mutex_lock(&log_lk);(intr_idx_+=sprintf(intr_log_+intr_idx_,"\n[cpu%d]%s:%d %s(%d):%s %d",cpu_id,__FILE__,__LINE__,tasks[currents[cpu_id]]->name,currents[cpu_id],info,ncli[cpu_id]));pthread_mutex_unlock(&log_lk);intr_idx_&=(1<<16)-1;}
     #define detail_log(_log,_idx,info) \
         do{ \
-            int cpu_id=_cpu(),_old=_idx; \
+            int cpu_id=_cpu(); \
             _idx+=sprintf(_log+_idx,"\n[cpu%d]%s:%3d(%s) %s:%s",_cpu(),__FILE__,__LINE__,__func__,tasks[current]->name,info); \
-            (void)_old; \
-            tasks_old=_old; \
-            /*printf(_log+_old);*/ \
             _idx&=(1<<16)-1; \
         }while(0)
 #else
