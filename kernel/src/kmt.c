97f1ad1003824ce093340a252714b4561f861d7d
tracer-nju <tracer@nju.edu.cn>
2019-07-03 15:04:54 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: fa1d2a945032465c926be6e39230d283   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-19-generic       Architecture: x86-64  15:04:54 up 2 days,  5:57,  7 users,  load average: 0.85, 0.80, 0.83

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -89,6 +89,7 @@ static _Context* kmt_context_switch(_Event ev, _Context *c){
             Assert(_intr_read()==0,"%d",cpu_id);
             if((current->attr&TASK_SLEEP)==0)
                 return NULL;
+            current=NULL;
             return &idles[cpu_id].context;
         }
     }while(tasks[new]->attr ||
