1103b49acba8f376aa975da081758c729eed6be7
tracer-nju <tracer@nju.edu.cn>
2019-07-06 18:26:36 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  18:26:36 up  5:51, 10 users,  load average: 1.06, 0.92, 0.99

--- kernel/src/shell/buildin/cat.c
+++ kernel/src/shell/buildin/cat.c
@@ -4,7 +4,7 @@ const char *warn="Haven't realized!\n";
 
 static inline void cat_from_stdin(char buf[0x200]){
     int nread=0;
-    while((nread=vfs->read(STDIN,buf,sizeof(buf)))){
+    while((nread=vfs->read(STDIN,buf,0x200-1))){
         if(nread==0)return 0;
         buf[nread]='\0';
         std_write(buf);
