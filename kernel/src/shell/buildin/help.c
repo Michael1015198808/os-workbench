713fd35abdd91de9a207a66f0f62f9ae4f318482
tracer-nju <tracer@nju.edu.cn>
2019-07-06 18:41:57 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  18:41:57 up  6:06, 10 users,  load average: 0.82, 0.79, 0.86

--- kernel/src/shell/buildin/help.c
+++ kernel/src/shell/buildin/help.c
@@ -4,9 +4,9 @@
 int mysh_help(void *args[]){
     for(int i=0;args[i];++i){
         const char* const buildin_help(void*);
-        const char* const help=buildin_help(args[i]);
+        void* help=buildin_help(args[i]);
         if(help){
-            std_write((char*)help);
+            std_write(help);
         }else{
             std_write("help: no help topics match `");
             std_write(args[i]);
