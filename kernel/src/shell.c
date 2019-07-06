1f353bfa3651da805081626db469c94404c0210a
tracer-nju <tracer@nju.edu.cn>
2019-07-06 12:48:31 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  12:48:32 up 13 min,  5 users,  load average: 1.13, 0.95, 0.64

--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -33,41 +33,29 @@ static void inline command_handler(char *input,void *args[10],int nread){
     }while(0);
 }
 void mysh(void *name) {
-    {
-        /*int temp=vfs->open(name,7);
-        Assert(temp==0,"Error on fd-stdin:%d",temp);
-        temp=vfs->open(name,7);
-        Assert(temp==1,"Error on fd-stdout:%d",temp);*/
-    }
-    device_t* tty=dev_lookup(name);//For debugging
+    device_t *tty = dev_lookup(name);
     while (1) {
         char input[128], prompt[128];
         void *args[10];
         sprintf(prompt, "(%s) $ ", name);
         tty_write(tty,prompt);
-        //vfs->write(1,info(prompt));
-        int nread = tty->ops->read(tty,0,info(input));
-        //int nread = vfs->read(0,info(input));
-        input[nread-1]='\0';
-
-        printf("%s\n",input);
-        command_handler(input,args,nread);
-        for(int i=0;args[i];++i){
+        //tty->ops->write(tty, 0, prompt, strlen(prompt)+1);
+        int nread = tty->ops->read(tty, 0, input, sizeof(input));
+        
+        command_handler(input,args);
+        /*for(int i=0;args[i];++i){
             printf("args[%d]:%s\n",i,args[i]);
-        }
+        }*/
         for(int i=0;;++i){
             if(i==LEN(buildin)){
                 char warn[]="mysh: command not found: ";
                 tty_write(tty,warn);
                 tty_write(tty,args[0]);
                 tty_write(tty,"\n");
-                //vfs->write(1,info(warn));
-                //vfs->write(1,info(args[0]));
-                //vfs->write(1,info("\n"));
                 break;
             }else
             if(!strcmp(input,buildin[i].name)){
-                buildin[i].binary(args);
+                buildin[i].binary(args,tty);
                 break;
             }
         }
