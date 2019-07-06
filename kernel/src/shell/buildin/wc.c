9ed6fe5b1164eb0f2c63eeeca63d9e92b4ebbc3c
tracer-nju <tracer@nju.edu.cn>
2019-07-06 19:23:44 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  19:23:44 up  6:48, 10 users,  load average: 0.85, 0.67, 0.76

--- kernel/src/shell/buildin/wc.c
+++ kernel/src/shell/buildin/wc.c
@@ -1,29 +1,33 @@
 #include <klib.h>
 #include <devices.h>
 
-inline void single_wc(int fd, char *name,int cnt[3]){
+inline void single_wc(int fd, char *name,int total[3]){
     char buf[0x200];
     int i,n;
-    int l=0,w=0,c=0,inword=0;
+    int cnt[3],inword=0;
+    //cnt[3]:=(l,w,c)
+    //line, word, character
 
     while((n = std_read(fd, buf, sizeof(buf))) > 0){
-        for(i=0; i<n; i++){
-            c++;
-            if(buf[i] == '\n')
-                l++;
+        for(i=0;i<n;++cnt[2],++i){
+            cnt[0]+=(buf[i]=='\n');
+
             if(strchr(" \r\t\n\v", buf[i]))
                 inword = 0;
             else if(!inword){
-                w++;
+                ++cnt[1];
                 inword = 1;
             }
         }
     }
     if(n < 0){
-        std_write(1, "wc: read error\n");
+        vfs->write(2, winfo("wc: read error\n"));
         exit();
     }
     std_write(1, "%4d %4d %4d %s\n", l, w, c, name);
+    for(int i=0;i<3;++i){
+        total[i]+=cnt[i];
+    }
 }
 
 int mysh_wc(char *argv[]){
@@ -35,7 +39,7 @@ int mysh_wc(char *argv[]){
                 printf(1, "wc: cannot open %s\n", argv[i]);
                 return -1;
             }
-            single_wc(fd, argv[i]);
+            single_wc(fd, argv[i],cnt);
             close(fd);
         }
         for(int i=0;i<3;++i){
@@ -44,7 +48,7 @@ int mysh_wc(char *argv[]){
         }
         std_write("total\n");
     }else{
-        single_wc(0, "");
+        single_wc(0, "",cnt);
     }
     return 0;
 }
