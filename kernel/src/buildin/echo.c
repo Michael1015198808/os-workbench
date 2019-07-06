bc590a847dc57fe47fa99e6ea252ff50fbf223c9
tracer-nju <tracer@nju.edu.cn>
2019-07-06 13:06:32 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  13:06:32 up 31 min,  5 users,  load average: 0.76, 0.87, 0.80

--- kernel/src/buildin/echo.c
+++ kernel/src/buildin/echo.c
@@ -4,12 +4,12 @@
 int echo(void *args[],device_t *dev){
     int i=1;
     while(1){
-        std_write(0,winfo(args[i]));
+        std_write(winfo(args[i]));
         ++i;
         if(args[i]){
-            std_write(0,winfo(" "));
+            std_write(winfo(" "));
         }else{
-            std_write(0,winfo("\n"));
+            std_write(winfo("\n"));
             return 0;
         }
     }
