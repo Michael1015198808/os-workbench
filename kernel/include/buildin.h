1189d0a923c74807d80f15ce6a181c5e13c133e1
tracer-nju <tracer@nju.edu.cn>
2019-07-06 13:22:08 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  13:22:08 up 47 min,  6 users,  load average: 1.00, 0.84, 0.93

--- kernel/include/buildin.h
+++ kernel/include/buildin.h
@@ -4,7 +4,7 @@
 typedef int handler(void*[]);
 #define declare(function) \
     handler(function); \
-    const char* const MACRO_CONCAT(help_,MACRO_SELF(function))
+    extern const char* const MACRO_CONCAT(help_,MACRO_SELF(function))
 
 declare(echo);
 declare(cat);
