0b049c56aac7194685a0174ff83b227593078cde
tracer-nju <tracer@nju.edu.cn>
2019-07-07 11:00:41 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  11:00:41 up 22:25, 10 users,  load average: 2.61, 2.05, 1.47

--- kernel/src/shell/buildin/echo.c
+++ kernel/src/shell/buildin/echo.c
@@ -3,7 +3,9 @@
 
 int mysh_echo(void *args[]){
     int i=1;
-    if(!args[1])std_write("\n"),return 0;
+    if(!args[1]){
+        std_write("\n");
+    }else
     while(1){
         std_write(args[i]);
         ++i;
