c188951058bca3cd2e83ae172da1f0ad921fe1e8
tracer-nju <tracer@nju.edu.cn>
2019-07-07 11:16:17 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  11:16:17 up 22:41, 10 users,  load average: 0.83, 1.04, 1.22

--- kernel/src/shell/buildin/cat.c
+++ kernel/src/shell/buildin/cat.c
@@ -2,22 +2,23 @@
 #include <devices.h>
 const char *warn="Haven't realized!\n";
 
-static inline void cat_from_stdin(char buf[0x200]){
+static inline void cat_from_stdin(char buf[0x200],int* err){
     int nread=0;
     while((nread=vfs->read(STDIN,buf,0x200-1))>0){
         printf("cat read %d bytes\n",nread);
         buf[nread]='\0';
         std_write(buf);
     }
-    return (nread==0)?0:-1;
+    if(nread<0)*err=-1;
 }
 
 int mysh_cat(void *args[]){
     char buf[0x200];
+    int err=0;
     if(args[1]){
         for(int i=1;args[i];++i){
             if(strcmp(args[i],"-")){
-                cat_from_stdin(buf);
+                cat_from_stdin(buf,&err);
             }else{
                 int fd=vfs->open(args[i],7),nread=0;
                 do{
@@ -27,7 +28,7 @@ int mysh_cat(void *args[]){
             }
         }
     }else{
-        cat_from_stdin(buf);
+        cat_from_stdin(buf,&err);
     }
-    return 1;
+    return *err;
 }
