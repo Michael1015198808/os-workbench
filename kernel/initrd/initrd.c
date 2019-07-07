c6486bdf6a8a8e0172c1accb6e9b757852ed2129
鄢振宇 <171240518@smail.nju.edu.cn>
2019-07-07 19:31:14 +0800

    Finish ls vfs_open, yls_lookup, yls_iopen

--- kernel/initrd/initrd.c
+++ kernel/initrd/initrd.c
@@ -7,6 +7,7 @@ const char* const FMT="%02x ";
 const char* const FMT="%c";
 #endif
 
+#define CHECK_CODE 0x0a736c79
 void write_xxd(uint32_t info){
     uint8_t *c=(void*)&info;
     for(int i=0;i<4;++i){
@@ -23,17 +24,17 @@ struct pair{
     uint32_t off,info;
 }pairs[]={
     {0x000,0x00000210},
-    {0x100,0x00000000},
-    {0x104,0x00000110},
-    {0x108,0x00000150},
-    {0x10c,0x0a736c79},
-    {0x110,0x00000190},
-    {0x150,'/'},
-    {0x190,0x00000000},
-    {0x194,0x000001a0},
-    {0x198,0x000001e0},
-    {0x19c,0x0a736c79},
-    {0x1e0,0x74736574}
+    {0x100,0x00000000},//"/"
+    {0x104,0x00000110},//"/"'s info's offset
+    {0x108,0x02100150},//"/"'s name's offset
+    {0x10c,CHECK_CODE},
+    {0x110,0x00000190},//"/"'s info
+    {0x150,       '/'},//"/"'s name'
+    {0x190,0x00000000},//"/test"
+    {0x194,0x000001a0},//"/test"'s info's offset
+    {0x198,0x000001e0},//"/test"'s name's offset
+    {0x19c,CHECK_CODE},
+    {0x1e0,0x74736574},//"/test"'s name 
 };
 int main(){
     for(uint32_t i=0,j=0;i<0x300;){
