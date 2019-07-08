d5c8bfe3e720cf3c36966824379646af94c662c5
tracer-nju <tracer@nju.edu.cn>
2019-07-08 20:58:04 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  20:58:04 up 2 days,  8:23, 10 users,  load average: 0.65, 0.83, 0.83

--- kernel/include/dir.h
+++ kernel/include/dir.h
@@ -5,6 +5,6 @@ int get_first_slash_from(const char* const path,int idx);
 int get_first_slash(const char* const path);
 int get_last_slash_from (const char* const path,int idx);
 int get_last_slash (const char* const path);
-void dir_cat(char* dest,const char* src);
+void dir_cat(char* dest,char* src);
 
 #endif//__DIR_H__
