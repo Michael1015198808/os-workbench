e74077688698f013444e16fc0238d1757db6385c
tracer-nju <tracer@nju.edu.cn>
2019-07-08 23:01:57 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  23:01:57 up 2 days, 10:26, 10 users,  load average: 1.67, 1.17, 0.96

--- kernel/src/dir.c
+++ kernel/src/dir.c
@@ -22,7 +22,7 @@ int get_last_slash(const char* const path){
 static inline void dir_cat_real(char* dest,const char* src){
     while(src){
         int next=get_first_slash(src),flag=0;
-        if(next==-1)next=strlen(src);
+        if(next==-1)next=strlen(src)-1;
         switch(next){
             case 0:
                 break;
