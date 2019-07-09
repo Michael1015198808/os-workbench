5186a1b1875729f10a3891bafc0d79581a15ea67
tracer-nju <tracer@nju.edu.cn>
2019-07-09 13:39:26 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: ca95c486da1f4c48b18b8e1855636565   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  13:39:27 up 3 days,  1:04, 10 users,  load average: 1.13, 1.20, 1.49

---
+++ kernel/src/yls/path.c
@@ -0,0 +1,38 @@
+#include <common.h>
+#include <yls.h>
+
+inline int find_path(device_t* dev,yls_node* cur,const char* path){
+    int ret,path_len=strlen(path)-1;//Starts from "/"
+    ssize_t(*const read)(device_t*,off_t,void*,size_t)=fs->dev->ops->read;
+
+    while(ret=0;path[ret];++ret){
+        uint32_t off=cur->info;
+        if(cur->type!=YLS_DIR){
+            fprintf(2,"%s: not a directory\n",path);
+        }
+        while(1){
+            for(int i=0;i<OFFS_PER_MEM;++i,off+=4){
+                uint32_t next_off;
+                //Get next's yls_node from off
+                read(dev,off,&next_off,4);
+                if(!file_cmp(dev,next_off,path)){
+                    int len=get_first_slash(path);
+                    read(dev,next_off,cur,12);
+                    if(len==-1){
+                        path_len=0;
+                    }else{
+                        path_len-=len+1;
+                        path    +=len+1;
+                    }
+                    goto found;
+                }
+            }
+            read(dev,off,&off,4);
+            if(!off){
+                return ret;
+            }
+        }
+found:;
+    }
+    return ret;
+}
