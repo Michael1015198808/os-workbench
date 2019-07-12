3ee8cd6440e00dbadb079a022248eb09e5e715c7
tracer-nju <tracer@nju.edu.cn>
2019-07-12 11:14:04 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: cfd6e2c53b2a4811962f71a71a49132e   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  11:14:04 up 2 days,  2:16, 10 users,  load average: 0.58, 0.53, 0.58

--- kernel/src/devices/dev.c
+++ kernel/src/devices/dev.c
@@ -4,15 +4,15 @@
 
 
 #define DEVICES(_) \
-  _(0, rd_t,    "ramdisk0", 1, &rd_ops)     \
-  _(1, rd_t,    "ramdisk1", 2, &rd_ops)     \
-  _(2, input_t, "input",    1, &input_ops)  \
-  _(3, fb_t,    "fb",       1, &fb_ops)     \
-  _(4, tty_t,   "tty1",     1, &tty_ops)    \
-  _(5, tty_t,   "tty2",     2, &tty_ops)    \
-  _(6, tty_t,   "tty3",     3, &tty_ops)    \
-  _(7, tty_t,   "tty4",     4, &tty_ops)    \
-  _(8, void,    "null",     1, &null_ops)
+  _(0, rd_t,    "ramdisk0", 1, rd_ops)     \
+  _(1, rd_t,    "ramdisk1", 2, rd_ops)     \
+  _(2, input_t, "input",    1, input_ops)  \
+  _(3, fb_t,    "fb",       1, fb_ops)     \
+  _(4, tty_t,   "tty1",     1, tty_ops)    \
+  _(5, tty_t,   "tty2",     2, tty_ops)    \
+  _(6, tty_t,   "tty3",     3, tty_ops)    \
+  _(7, tty_t,   "tty4",     4, tty_ops)    \
+  _(8, void,    "null",     1, null_ops)
 
 #define OPS_DECLARE(id,device_type,dev_name,dev_id,dev_ops) \
     extern devops_t dev_ops;
@@ -30,7 +30,7 @@ device_t *dev_lookup(const char *name) {
   return NULL;
 }
 
-static device_t *dev_create(size_t dev_size, const char* dev_name, int dev_id, devops_t *dev_ops) {
+static inline device_t *dev_create(size_t dev_size, const char* dev_name, int dev_id, devops_t *dev_ops) {
   void *data = pmm->alloc(dev_size);
   device_t *dev = pmm->alloc(sizeof(device_t));
   *dev = (device_t) {
@@ -46,7 +46,7 @@ void tty_task(void *arg);
 void input_task(void *arg);
 
 #define CREATE(id, device_type, dev_name, dev_id, dev_ops) \
-  devices[id] = dev_create(sizeof(device_type), dev_name, dev_id, dev_ops);
+  devices[id] = dev_create(sizeof(device_type), dev_name, dev_id, &dev_ops);
 
 #define INIT(id, device_type, dev_name, dev_id, dev_ops) \
   devices[id]->ops->init(devices[id]);
