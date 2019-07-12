b94c2936d3405d107a310685432da377a8561075
tracer-nju <tracer@nju.edu.cn>
2019-07-12 17:26:08 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 7cec03eb15ef4e0cb118b74ea3b4868f   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  17:26:08 up 27 min,  7 users,  load average: 0.72, 0.98, 0.94

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -18,33 +18,6 @@
 
 #include <my_trace.h>
 
-#define winfo(string) \
-    (string), strlen((string))
-
-#define rinfo(string) \
-    (string), sizeof((string))
-
-#define TODO() Assert(0,"Haven't be realized!\n");
-#define MACRO_VALUE(_arg)     #_arg
-#define MACRO_SELF(_arg)      _arg
-#define MACRO_CONCAT_REAL(_arg1,_arg2) _arg1 ## _arg2
-#define MACRO_CONCAT(_arg1,_arg2) MACRO_CONCAT_REAL(_arg1,_arg2)
-#define TO_STRING(_arg)     MACRO_VALUE(_arg)
-
-#define _LOCAL
-
-#ifdef _LOCAL
-    #define local_log(...) log(__VA_ARGS__)
-#else
-    #define local_log(...) 
-#endif//_LOCAL
-
-#define LEN(arr) ((sizeof(arr) / sizeof(arr[0])))
-
-#define new(A) (typeof(A)*)pmm->alloc(sizeof(A))
-
-#define TASK_FENCE
-
 #define STK_SZ ((1<<12)-64)
 volatile int ncli[MAX_CPU],intena[MAX_CPU];
 
