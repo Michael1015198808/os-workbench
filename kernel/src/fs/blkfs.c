2e9d9d2cf36cd2a4939600a9965084db5adbbb35
tracer-nju <tracer@nju.edu.cn>
2019-07-13 22:31:07 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 43bc7268eeca45e7be1c966efa4fb716   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  22:31:07 up 11:44,  6 users,  load average: 0.60, 0.77, 0.74

--- kernel/src/fs/blkfs.c
+++ kernel/src/fs/blkfs.c
@@ -90,10 +90,10 @@ static int blkfs_iclose(vfile_t* file){
 }
 
 static ssize_t inline blkfs_iread_real(vfile_t* file,char* buf,size_t size){
-    filesystem* fs  =file->inode->fs;
-    uint64_t fd_off =file->offset;
-    yls_node* node  =file->inode->ptr;
-    uint32_t off=node->info;
+    filesystem* fs  = file->inode->fs;
+    uint64_t fd_off = file->offset;
+    yls_node* node  = file->inode->ptr;
+    uint32_t off    = node->info;
 
     switch(node->type){
         case YLS_DIR:
@@ -101,7 +101,7 @@ static ssize_t inline blkfs_iread_real(vfile_t* file,char* buf,size_t size){
             break;
         case YLS_FILE:
             {
-                find_block(fs->dev,off,&fd_off);
+                find_block(fs->dev,&off,&fd_off);
                 return block_read(fs->dev,off,fd_off,buf,size);
             }
             break;
