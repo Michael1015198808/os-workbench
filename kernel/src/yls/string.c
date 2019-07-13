5ba53e8438129b0ece0be83b2c8d128a95e0fe76
tracer-nju <tracer@nju.edu.cn>
2019-07-13 16:56:38 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 43bc7268eeca45e7be1c966efa4fb716   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  16:56:38 up  6:10,  5 users,  load average: 0.67, 0.64, 0.57

--- kernel/src/yls/string.c
+++ kernel/src/yls/string.c
@@ -26,9 +26,9 @@ int block_read (device_t* dev,uint32_t off,uint32_t shift,char* s,size_t nbyte){
     printf("read from %x,%x ",off,shift);
     off+=shift;
     printf("%x\n",off);
-    printf("%x,%x\n",(-off)&BLK_SZ,rest);
+    printf("%x,%x\n",BLK_MEM-(off)%BLK_SZ,rest);
     while(rest>0){
-        int to_read=min((-off)&BLK_SZ,rest);
+        int to_read=min(BLK_MEM-(off)%BLK_SZ,rest);
         if(read(dev,off,s,to_read)!=to_read){
             return nbyte-rest;
         }
