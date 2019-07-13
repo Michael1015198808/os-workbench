8e7afe55b565eabfc30172652a16294c2e61f85a
tracer-nju <tracer@nju.edu.cn>
2019-07-13 22:26:15 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 43bc7268eeca45e7be1c966efa4fb716   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  22:26:15 up 11:39,  6 users,  load average: 1.45, 1.01, 0.78

--- kernel/src/shell/buildin/cat.c
+++ kernel/src/shell/buildin/cat.c
@@ -5,11 +5,14 @@
 
 const char *warn="Haven't realized!\n";
 
-static inline void cat_from_stdin(char buf[0x200],int* err){
+static void single_cat(int fd,char buf[0x200],int* err){
     int nread=0;
-    while((nread=vfs->read(STDIN,buf,0x200-1))>0){
+    while((nread=vfs->read(fd,buf,0x200-1))>0){
         buf[nread]='\0';
         std_write(buf);
+    }
+    if(nread==EISDIR){
+        warn("%s: Is a directory",args[i]);
     }
     if(nread<0)*err=-1;
 }
@@ -24,19 +27,13 @@ int mysh_cat(void *args[]){
                 char file[0x100];
                 to_absolute(file,pwd,args[i]);
                 int fd=vfs->open(file,O_RDONLY),nread=0;
-                do{
-                    nread=vfs->read(fd,buf,sizeof(buf));
-                    vfs->write(STDOUT,buf,nread);
-                }while(nread>0);
-                if(nread==EISDIR){
-                    warn("%s: Is a directory",args[i]);
-                }
+                single_cat(fd,buf,&err);
             }else{
-                cat_from_stdin(buf,&err);
+                single_cat(0,buf,&err);
             }
         }
     }else{
-        cat_from_stdin(buf,&err);
+        single_cat(0,buf,&err);
     }
     return err;
 }
