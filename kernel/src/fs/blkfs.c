95bb770e3039b77d77b2b791a793c2c285ff1280
tracer-nju <tracer@nju.edu.cn>
2019-07-13 21:50:24 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 43bc7268eeca45e7be1c966efa4fb716   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  21:50:24 up 11:04,  6 users,  load average: 0.68, 0.64, 0.61

--- kernel/src/fs/blkfs.c
+++ kernel/src/fs/blkfs.c
@@ -90,6 +90,7 @@ static int blkfs_iclose(vfile_t* file){
 }
 
 static ssize_t inline blkfs_iread_real(vfile_t* file,char* buf,size_t size){
+    ssize_t ret=0;
 
     filesystem* fs  =file->inode->fs;
     uint64_t fd_off =file->offset;
@@ -99,7 +100,16 @@ static ssize_t inline blkfs_iread_real(vfile_t* file,char* buf,size_t size){
     switch(node->type){
         case YLS_DIR:
             {
-                TODO();
+                //Read a directory, you'll get
+                //names of directorys in it
+                //(So readdir is not needed)
+                if(block_read(fs->dev,off,fd_off,(void*)&off,4)!=4)return 0;
+                if(off==0)return 0;//find_block failed or reached the end
+
+                ssize_t nread=block_read(fs->dev,off,4,buf,size);
+                ret+=nread;
+                file->offset+=4;
+                return ret;
             }
             break;
         case YLS_FILE:
