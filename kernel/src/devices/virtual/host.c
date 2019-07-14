9e67f1b5d0076591876a2d9e6b3d1c0aff3a7d6a
tracer-nju <tracer@nju.edu.cn>
2019-07-14 16:25:05 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 86b679b757d54a529e568c8af9b9fae1   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  16:25:06 up 8 min,  5 users,  load average: 0.81, 0.77, 0.45

--- kernel/src/devices/virtual/host.c
+++ kernel/src/devices/virtual/host.c
@@ -7,6 +7,10 @@ static int host_init(device_t *dev) {
 static ssize_t host_read(device_t *dev, off_t offset, void *buf, size_t count) {
     extern uint8_t _getc(void);
     for(size_t i=0;i<count;++i){
+        uint8_t c;
+        do{
+            c=_getc();
+        }while(c=='\0');
         ((char*)buf)[i]=_getc();
     }
     return count;
