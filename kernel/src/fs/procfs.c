167045e602fd76798d393514f61d3ef9b38775ed
tracer-nju <tracer@nju.edu.cn>
2019-07-17 19:14:20 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 9698a8bcd9ff421c9ed25ee9580930a0   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  19:14:20 up 10:28,  7 users,  load average: 0.98, 0.86, 0.74

--- kernel/src/fs/procfs.c
+++ kernel/src/fs/procfs.c
@@ -140,10 +140,10 @@ static ssize_t procfs_ireaddir(vfile_t* file,char* buf,size_t size){
         }else{
             if(file->offset<3){
                 file->offset+=
-                    (nread=snprintf(buf,size,proc_info[0]+file->offset));
+                    (nread=snprintf(buf,size,proc_info[1]+file->offset));
             }else if(file->offset<7){
                 file->offset+=
-                    (nread=snprintf(buf,size,proc_info[1]+file->offset)-3);
+                    (nread=snprintf(buf,size,proc_info[2]+file->offset)-3);
             }
         }
     }
