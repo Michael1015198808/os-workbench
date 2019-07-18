813f81478839d0572c7eeef14b054083c5d2616b
tracer-nju <tracer@nju.edu.cn>
2019-07-18 14:39:13 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 7898abcf6e6846e8a5ea018b6bcd5690   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  14:39:13 up  2:46,  9 users,  load average: 0.64, 0.64, 0.59

--- kernel/src/shell/buildin/test.c
+++ kernel/src/shell/buildin/test.c
@@ -6,7 +6,7 @@
 int mysh_test(void *args[]){
     uint32_t begin=uptime();
     for(int i=1;i<=20;++i){
-        while(uptime()-begin<to_sleep)_yield();
+        while(uptime()-begin<1000)_yield();
         begin=uptime();
         fprintf(1,"Hello, world for the %d-th time!\n",i);
     }
