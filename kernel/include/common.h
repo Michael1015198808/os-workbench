5a42d4a5c41f52db99b2c5dcc64bb1bf4b623a08
tracer-nju <tracer@nju.edu.cn>
2019-07-19 15:35:59 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 90551f5467624e9384e164041e73a652   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  15:36:00 up  6:39,  6 users,  load average: 1.40, 1.53, 1.53

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -8,10 +8,6 @@
 #include <stdint.h>
 #include <macros.h>
 
-task_t* get_cur(void);
-
-#include <vfs.h>
-
 #ifndef MAX_CPU
     #define MAX_CPU 8
 #endif
@@ -33,6 +29,7 @@ volatile int ncli[MAX_CPU],intena[MAX_CPU];
     }while(0)
 
 typedef void(*task_fun)(void*);
+task_t* get_cur(void);
 
 int fprintf(int fd, const char *fmt, ...);
 void warning(const char*);
