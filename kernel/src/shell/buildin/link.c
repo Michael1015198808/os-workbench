be772aa105a53d49115d6a0b3c43d0edfb71546f
tracer-nju <tracer@nju.edu.cn>
2019-07-21 10:35:05 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: f645881889794a488d5936acc48718e9   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  10:35:05 up 55 min,  5 users,  load average: 0.47, 0.54, 0.63

--- kernel/src/shell/buildin/link.c
+++ kernel/src/shell/buildin/link.c
@@ -5,11 +5,10 @@
 #include <dir.h>
 
 int mysh_link(void *args[]){
-    char buf[0x208];
-    int err=0;
     if(args[1]&&args[2]&&!args[3]){
-        vfs->link(args[1],args[2]);
+        return vfs->link(args[1],args[2]);
     }else{
         fprintf(2,"Missing operand\nUsage: link FILE1 FILE2\n");
     }
+    return -1;
 }
