4db89d3b90db2d42c8f5c356a1858749c8a426e1
tracer-nju <tracer@nju.edu.cn>
2019-07-21 21:45:54 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: a5b533738967494ba0d6dff33d383f8f   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  21:45:54 up  1:48,  7 users,  load average: 0.69, 0.74, 0.91

--- kernel/src/tests/pool_test.c
+++ kernel/src/tests/pool_test.c
@@ -3,7 +3,7 @@
 #include <klib.h>
 
 #ifndef NO_TEST
-pool p[2];
+pool p[3]={};
 void queue1(void *arg){
     void* get;
     while(1){
@@ -24,9 +24,37 @@ void queue2(void *arg){
         enqueue(&p[1],arg);
     }
 }
+
+void pusher(void* arg){
+    for(int i=0;i<5;++i){
+        enqueue(p+2,arg+i);
+    }
+    while(1);
+}
+void receiver(void* arg){
+    char* get;
+    int a=0,A=0;
+    while(1){
+        do{
+            get=dequeue(p+2);
+        }while(!get);
+        if(*get-'a'<5&&*get-'a'>=0){
+            Assert('a'+a==*get,"Test failed!");
+            ++a;
+            if(a==5)a=0;
+        }else{
+            Assert('A'+A==*get,"Test failed!");
+            ++A;
+            if(A==5)A=0;
+        }
+    }
+}
 void queue_test_init(void){
-    kmt->create(pmm->alloc(sizeof(task_t)), "hello1", queue1, "a");
-    kmt->create(pmm->alloc(sizeof(task_t)), "hello2", queue2, "b");
+    kmt->create(pmm->alloc(sizeof(task_t)),"hello1"  ,queue1  ,"a"    );
+    kmt->create(pmm->alloc(sizeof(task_t)),"hello2"  ,queue2  ,"b"    );
+    kmt->create(pmm->alloc(sizeof(task_t)),"pusher1" ,pusher  ,"ABCDE");
+    kmt->create(pmm->alloc(sizeof(task_t)),"pusher2" ,pusher  ,"abcde");
+    kmt->create(pmm->alloc(sizeof(task_t)),"receiver",receiver,NULL   );
 }
 #endif
 
