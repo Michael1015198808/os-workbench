941629fef246c5d1e454bcdb295a7109ea55269f
tracer-nju <tracer@nju.edu.cn>
2019-07-22 17:11:22 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: c01863d98795467b9e4970856a54eafc   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  17:11:22 up  6:11,  6 users,  load average: 0.75, 0.97, 0.96

--- kernel/src/shell/buildin/color.c
+++ kernel/src/shell/buildin/color.c
@@ -10,14 +10,26 @@ static inline int set_color_real(void *args[],device_t* tty,uint32_t* colors){
         strlen(args[1]),strlen(args[2])
     };
 
+    if(!args[1]){
+            colors[0]=DEFAULT_FG;
+            colors[1]=DEFAULT_BG;
+    }
     if(args[1]&&args[2]&&!args[3]){
-        if(len[0]==6&&len[1]==6){
+        if(len[0]==2){
+            if(!strcmp(args[1],"bg")){
+                colors[0]=atoi16(args[2]);
+                return 0;
+            }else if(!strcmp(args[1],"fg")){
+                colors[1]=atoi16(args[2]);
+                return 0;
+            }
+        }else if(len[0]==6&&len[1]==6){
             colors[0]=atoi16(args[1]);
             colors[1]=atoi16(args[2]);
             return 0;
         }
     }
-    fprintf(2,"Usage: color [fg|bg] COLOR\n  or:  color COLOR COLOR\nFORMAT: ffffff\n");
+    fprintf(2,"Usage: color\n  or:  color [fg|bg] COLOR\n  or:  color COLOR COLOR\nFORMAT: ffffff\n");
     return -1;
 }
 
