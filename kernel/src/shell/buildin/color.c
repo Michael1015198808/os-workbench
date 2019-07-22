2e3b3a613203d4a88a00efead79a368c90fdbe3e
tracer-nju <tracer@nju.edu.cn>
2019-07-22 17:15:16 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: c01863d98795467b9e4970856a54eafc   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-20-generic       Architecture: x86-64  17:15:17 up  6:15,  6 users,  load average: 1.04, 1.05, 1.00

--- kernel/src/shell/buildin/color.c
+++ kernel/src/shell/buildin/color.c
@@ -18,10 +18,10 @@ static inline int set_color_real(void *args[],device_t* tty,uint32_t* colors){
     if(args[1]&&args[2]&&!args[3]){
         if(len[0]==2){
             if(!strcmp(args[1],"bg")){
-                colors[0]=atoi16(args[2]);
+                colors[1]=atoi16(args[2]);
                 return 0;
             }else if(!strcmp(args[1],"fg")){
-                colors[1]=atoi16(args[2]);
+                colors[0]=atoi16(args[2]);
                 return 0;
             }
         }else if(len[0]==6&&len[1]==6){
