60f2811c0d058ca3965c5b76cef6cc93151ea7c8
tracer-nju <tracer@nju.edu.cn>
2019-07-30 12:40:29 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 3fde73778a7e479abeb20bdb7a36fd8a   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-21-generic       Architecture: x86-64  12:40:29 up 18 min,  4 users,  load average: 0.50, 0.65, 0.60

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -124,7 +124,7 @@ void make_tree(void){
             Assert(snprintf(filename,"%s%s",entry->d_name,"/task")>0,"process's name is too long");
             Assert( (tasks= opendir(filename)) ,  "Can not open /proc/%s\n",filename);
             while((task_entry = readdir(tasks)) != NULL) {if(digit_judge(task_entry->d_name)) {
-                Assert(sprintf(filename,"%s%s%s%s",entry->d_name,"/task/",task_entry->d_name,"/status")>0,"process's name is too long");
+                Assert(snprintf(filename,sizeof(filename)-1,"%s%s%s%s",entry->d_name,"/task/",task_entry->d_name,"/status")>0,"process's name is too long");
 #ifdef IGNORE_PRO_EXIT
                 if((fp=fopen(filename,"r"))==NULL)continue;
 #else
