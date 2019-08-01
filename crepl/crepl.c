2de4a58c4d87d4c7ec3de928b528351c214d2298
tracer-nju <tracer@nju.edu.cn>
2019-08-01 15:00:35 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 8c31200a3bab48429668da42ee3d0db0   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-21-generic       Architecture: x86-64  15:00:36 up  4:24,  3 users,  load average: 0.21, 0.20, 0.30

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -25,21 +25,33 @@ int dprintf(int fd,const char* format,...);
 #define CC \
     "/usr/bin/gcc"
 
+#define Assert(cond, _fmt, ...) \
+    do{ \
+        if(!(cond)){ \
+            fprintf(stderr, _fmt "\n" ## __VA_ARGS__); \
+            exit(1); \
+        } \
+    }while(0)
+
 #define err(...) \
     fprintf(stderr, __VA_ARGS__)
 
 #define log(format,...) \
     err("Line %3d: " format,__LINE__, __VA_ARGS__)
 
-char *cmd=NULL,out[16],src[8];
+#if defined(__i386__)
+    #define MACHINE_OPTION "-m32"
+#elif defined(__x86_64__)
+    #define MACHINE_OPTION "-m64"
+#else
+    _Static_assert(0,"Unknown architecture");
+#endif
+
+char *cmd=NULL,out[12],src[7];
 char *cflags[]={
     "gcc",
     "-fPIC",
-#if defined(__i386__)
-    "-m32",
-#elif defined(__x86_64__)
-    "-m64",
-#endif
+    MACHINE_OPTION,
     //"-Werror",
     "-shared",
     "-x",
@@ -49,7 +61,6 @@ char *cflags[]={
     src,
     NULL
 };
-void *handle;
 int main(int argc, char *argv[],char *envp[]) {
     while(1){
         //Input
@@ -59,49 +70,53 @@ int main(int argc, char *argv[],char *envp[]) {
             add_history(cmd);
         }
         if(!strcmp("exit",cmd))return 0;
-    
+
         //Create temp file
         char file[]="XXXXXX";
         int fd=mkstemp(file);
-        if(fd>0){
+        if(fd<0){
+            log("%s","Can't create temporary file!\n");
+        }else{
             int add_func=!strncmp("int",cmd,3);
             if(add_func){
                 dprintf(fd,"%s",cmd);
             }else{
                 dprintf(fd,"int __expr_wrapper(){return %s;}",cmd);
             }
-        
+
             //Compile and link
-            strcpy(src,file);
-            sprintf(out,"./%s.so",src);
-            if(!fork()){
-                close(1);close(2);
+            sprintf(out,"./%s.so",file);
+            int pid=fork();
+            if(pid<0)err("Fork failed!");
+            if(pid==0){
+                strcpy(src,file);
                 execve(CC,cflags,envp);
-            }
-            uint32_t wstatus;
-            wait((void*)&wstatus);
-            unlink(file);
-            if(wstatus!=0){
-                err("%d" ":%s",wstatus, "Compile error!\n");
-                continue;
-            }
-            handle=dlopen(out, RTLD_LAZY|RTLD_GLOBAL);
-            unlink(out);
-            if(!handle){
-                err("load failed!\n");
-                continue;
-            }
-            if(add_func){
-                //Add a function
-                printf("Added: %s\n",cmd);
+                err("call " CC " failed!\n");
+                return -1;
             }else{
-                //Calculate the value
-                int (*fun)(void)= dlsym(handle, "__expr_wrapper");
-                assert(fun);
-                printf("(%s) == %d\n",cmd,fun());
+                uint32_t wstatus;
+                wait((void*)&wstatus);
+                unlink(file);
+                if(wstatus!=0){
+                    err("%d" ":%s",wstatus, "Compile error!\n");
+                    continue;
+                }
+                void *handle=dlopen(out, RTLD_LAZY|RTLD_GLOBAL);
+                unlink(out);
+                if(!handle){
+                    err("load failed!\n");
+                    continue;
+                }
+                if(add_func){
+                    //Add a function
+                    printf("Added: %s\n",cmd);
+                }else{
+                    //Calculate the value
+                    int (*fun)(void)= dlsym(handle, "__expr_wrapper");
+                    assert(fun);
+                    printf("(%s) == %d\n",cmd,fun());
+                }
             }
-        }else{
-            log("%s","Can't create temporary file!\n");
         }
     }
     return 0;
