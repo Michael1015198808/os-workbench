4a833ee021f3b0acad5c21b776255fb1f9db8e0e
tracer-nju <tracer@nju.edu.cn>
2019-08-01 14:35:14 +0800

    Static hostname: neco          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7f089d45c60d43dc9d7c91ef53ef5bb7            Boot ID: 8c31200a3bab48429668da42ee3d0db0   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-21-generic       Architecture: x86-64  14:35:15 up  3:59,  3 users,  load average: 0.45, 0.52, 0.52

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -20,12 +20,17 @@
 #include <sys/wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+int dprintf(int fd,const char* format,...);
+
 #define CC \
     "/usr/bin/gcc"
+
+#define err(...) \
+    fprintf(stderr, __VA_ARGS__)
+
 #define log(format,...) \
-    fprintf(stderr,"Line %3d: " format,__LINE__, __VA_ARGS__)
-#define my_write(_fd,_str) \
-    write(_fd,_str,strlen(_str))
+    err("Line %3d: " format,__LINE__, __VA_ARGS__)
+
 char *cmd=NULL,out[16],src[8];
 char *cflags[]={
     "gcc",
@@ -44,71 +49,60 @@ char *cflags[]={
     src,
     NULL
 };
-int suffix_of(char *,char *);
 void *handle;
-/*sigjmp_buf interpreter;
-void restore(int errno){
-    longjmp(interpreter,-1);
-}*/
 int main(int argc, char *argv[],char *envp[]) {
-  //signal(SIGSEGV,restore);
-  //setjmp(interpreter);
-  while(1){
-    //Input
-    if(cmd){free(cmd);cmd=NULL;}
-    cmd=readline(">> ");
-    if(cmd&&*cmd){
-        add_history(cmd);
+    while(1){
+        //Input
+        free(cmd);
+        cmd=readline(">> ");
+        if(cmd&&*cmd){
+            add_history(cmd);
+        }
+        if(!strcmp("exit",cmd))return 0;
+    
+        //Create temp file
+        char file[]="XXXXXX";
+        int fd=mkstemp(file);
+        if(fd>0){
+            int add_func=!strncmp("int",cmd,3);
+            if(add_func){
+                dprintf(fd,"%s",cmd);
+            }else{
+                dprintf(fd,"int __expr_wrapper(){return %s;}",cmd);
+            }
+        
+            //Compile and link
+            strcpy(src,file);
+            sprintf(out,"./%s.so",src);
+            if(!fork()){
+                close(1);close(2);
+                execve(CC,cflags,envp);
+            }
+            uint32_t wstatus;
+            wait((void*)&wstatus);
+            unlink(file);
+            if(wstatus!=0){
+                err("%d" ":%s",wstatus, "Compile error!\n");
+                continue;
+            }
+            handle=dlopen(out, RTLD_LAZY|RTLD_GLOBAL);
+            unlink(out);
+            if(!handle){
+                err("load failed!\n");
+                continue;
+            }
+            if(add_func){
+                //Add a function
+                printf("Added: %s\n",cmd);
+            }else{
+                //Calculate the value
+                int (*fun)(void)= dlsym(handle, "__expr_wrapper");
+                assert(fun);
+                printf("(%s) == %d\n",cmd,fun());
+            }
+        }else{
+            log("%s","Can't create temporary file!\n");
+        }
     }
-    if(!strcmp("exit",cmd))return 0;
-
-    //Create temp file
-    char file[]="XXXXXX";
-    int fd=mkstemp(file);
-    int add_func=suffix_of("int",cmd);
-    if(fd==0)log("%s","Can't create temporary file!\n");
-    if(add_func){
-        my_write(fd,cmd);
-    }else{
-        my_write(fd,"int __expr_wrapper(){return ");
-        my_write(fd,cmd);
-        my_write(fd,";}");
-    }
-
-    //Compile and link
-    strcpy(src,file);
-    sprintf(out,"./%s.so",src);
-    if(!fork()){
-        close(1);close(2);
-        execve(CC,cflags,envp);
-    }
-    uint32_t wstatus;
-    wait((void*)&wstatus);
-    unlink(file);
-    if(wstatus!=0){
-        printf("%d" ":%s",wstatus, "Compile error!\n");
-        continue;
-    }
-    handle=dlopen(out, RTLD_LAZY|RTLD_GLOBAL);
-    if(!handle){
-        printf("load failed!\n");
-        continue;
-    }
-    if(add_func){
-        //Add a function
-        printf("Added: %s\n",cmd);
-    }else{
-        //Calculate the value
-        int (*fun)(void)= dlsym(handle, "__expr_wrapper");
-        assert(fun);
-        printf("(%s) == %d\n",cmd,fun());
-    }
-    unlink(out);
-  }
-  return 0;
-}
-int suffix_of(char *s1,char *s2){
-    while(*s1&&*s1==*s2)
-        ++s1,++s2;
-    return *s1=='\0';
+    return 0;
 }
